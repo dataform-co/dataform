be2db4f4b112b6cd73681effb22c30a8cbb52b97
Lewis Hemens <lewis@dataform.co>
2018-10-03 12:10:16 +0100

    Add config API and make sure multiple pre, post, assert, and dependency calls work

--- typescript/core/materialization.ts
+++ typescript/core/materialization.ts
@@ -5,6 +5,17 @@ import * as parser from "./parser";
 export type MContextable<T> = T | ((ctx: MaterializationContext) => T);
 export type MaterializationType = "table" | "view" | "incremental";
 
+export interface MConfig {
+  type: MaterializationType;
+  query: MContextable<string>;
+  where: MContextable<string>;
+  pre: MContextable<string | string[]>;
+  post: MContextable<string | string[]>;
+  assert: MContextable<string | string[]>;
+  dependencies: string | string[];
+  schema: { [key: string]: string } | string[];
+}
+
 export class Materialization {
   proto: protos.Materialization = protos.Materialization.create({
     type: "view"
@@ -16,9 +27,36 @@ export class Materialization {
   // We delay contextification until the final compile step, so hold these here for now.
   private contextableQuery: MContextable<string>;
   private contextableWhere: MContextable<string>;
-  private contextablePres: MContextable<string | string[]>;
-  private contextablePosts: MContextable<string | string[]>;
-  private contextableAssertions: MContextable<string | string[]>;
+  private contextablePres: MContextable<string | string[]>[] = [];
+  private contextablePosts: MContextable<string | string[]>[] = [];
+  private contextableAssertions: MContextable<string | string[]>[] = [];
+
+  public config(config: MConfig) {
+    if (config.type) {
+      this.type(config.type);
+    }
+    if (config.query) {
+      this.query(config.query);
+    }
+    if (config.where) {
+      this.where(config.where);
+    }
+    if (config.pre) {
+      this.pre(config.pre);
+    }
+    if (config.post) {
+      this.post(config.post);
+    }
+    if (config.assert) {
+      this.assert(config.assert);
+    }
+    if (config.dependencies) {
+      this.dependency(config.dependencies);
+    }
+    if (config.schema) {
+      this.schema(config.schema as any);
+    }
+  }
 
   public type(type: MaterializationType) {
     this.proto.type = type;
@@ -36,37 +74,46 @@ export class Materialization {
   }
 
   public pre(pres: MContextable<string | string[]>) {
-    this.contextablePres = pres;
+    this.contextablePres.push(pres);
     return this;
   }
 
   public post(posts: MContextable<string | string[]>) {
-    this.contextablePosts = posts;
+    this.contextablePosts.push(posts);
     return this;
   }
 
   public assert(query: MContextable<string | string[]>) {
-    this.contextableAssertions = query;
+    this.contextableAssertions.push(query);
   }
 
-  public dependency(value: string) {
-    this.proto.dependencies.push(value);
+  public dependency(value: string | string[]) {
+    this.proto.dependencies = (this.proto.dependencies || []).concat(
+      typeof value === "string" ? [value] : value
+    );
     return this;
   }
 
-  public describe(key: string, description: string);
-  public describe(map: { [key: string]: string });
-  public describe(
-    keyOrMap: string | { [key: string]: string },
+  public schema(key: string, description?: string);
+  public schema(map: { [key: string]: string });
+  public schema(keys: string[]);
+  public schema(
+    keyOrKeysOrMap: string | string[] | { [key: string]: string },
     description?: string
   ) {
-    if (!!this.proto.descriptions) {
-      this.proto.descriptions = {};
+    if (!!this.proto.schema) {
+      this.proto.schema = {};
     }
-    if (typeof keyOrMap === "string") {
-      this.proto.descriptions[keyOrMap] = description;
+    if (typeof keyOrKeysOrMap === "string") {
+      this.proto.schema[keyOrKeysOrMap] = description || "";
+    } else if (keyOrKeysOrMap instanceof Array) {
+      keyOrKeysOrMap.forEach(key => {
+        this.proto.schema[key] = "";
+      });
     } else {
-      Object.assign(this.proto.descriptions, keyOrMap);
+      Object.keys(keyOrKeysOrMap).forEach(key => {
+        this.proto.schema[key] = keyOrKeysOrMap[key] || "";
+      });
     }
   }
 
@@ -81,33 +128,38 @@ export class Materialization {
       this.contextableWhere = null;
     }
 
-    if (this.contextablePres) {
-      var appliedPres = context.apply(this.contextablePres);
-      this.proto.pres =
-        typeof appliedPres == "string" ? [appliedPres] : appliedPres;
-      this.contextablePres = null;
-    }
+    this.contextablePres.forEach(contextablePres => {
+      var appliedPres = context.apply(contextablePres);
+      this.proto.pres = (this.proto.pres || []).concat(
+        typeof appliedPres == "string" ? [appliedPres] : appliedPres
+      );
+    });
+    this.contextablePres = [];
 
-    if (this.contextablePosts) {
-      var appliedPosts = context.apply(this.contextablePosts);
-      this.proto.posts =
-        typeof appliedPosts == "string" ? [appliedPosts] : appliedPosts;
-      this.contextablePosts = null;
-    }
+    this.contextablePosts.forEach(contextablePosts => {
+      var appliedPosts = context.apply(contextablePosts);
+      this.proto.posts = (this.proto.posts || []).concat(
+        typeof appliedPosts == "string" ? [appliedPosts] : appliedPosts
+      );
+    });
+    this.contextablePosts = [];
 
-    if (this.contextableAssertions) {
-      var appliedAssertions = context.apply(this.contextableAssertions);
-      this.proto.assertions =
+    this.contextableAssertions.forEach(contextableAssertions => {
+      var appliedAssertions = context.apply(contextableAssertions);
+      this.proto.assertions = (this.proto.assertions || []).concat(
         typeof appliedAssertions == "string"
           ? [appliedAssertions]
-          : appliedAssertions;
-      this.contextableAssertions = null;
-    }
+          : appliedAssertions
+      );
+    });
+    this.contextableAssertions = [];
 
     // Compute columns.
     try {
       var tree = parser.parse(this.proto.query, {});
-      this.proto.parsedColumns = tree.statement[0].result.map(res => res.alias).map(column => column || "*");
+      this.proto.parsedColumns = tree.statement[0].result
+        .map(res => res.alias)
+        .map(column => column || "*");
     } catch (e) {
       // There was an exception parsing the columns, ignore.
     }
@@ -163,17 +215,20 @@ export class MaterializationContext {
     return "";
   }
 
-  public describe(key: string, description: string);
-  public describe(map: { [key: string]: string });
-  public describe(
-    keyOrMap: string | { [key: string]: string },
+  public schema(key: string, description?: string);
+  public schema(map: { [key: string]: string });
+  public schema(keys: string[]);
+  public schema(
+    keyOrKeysOrMap: string | string[] | { [key: string]: string },
     description?: string
   ) {
-    this.materialization.describe(keyOrMap as any, description);
-    if (typeof keyOrMap == "string") {
-      return keyOrMap;
-    }
+    this.materialization.schema(keyOrKeysOrMap as any, description);
     return "";
+  }
+
+  public describe(key: string, description?: string) {
+    this.materialization.schema(key, description);
+    return key;
   }
 
   public apply<T>(value: MContextable<T>): T {
--- typescript/example/datasets/sample_copy.sql
+++ typescript/example/datasets/sample_copy.sql
@@ -1,2 +1,9 @@
 ${type("table")}
+${config({
+  type: "table",
+  schema: {
+    sample: "Sample field.",
+    foobar: "Foobar field"
+  }
+})}
 select sample, "${macros.foo("bar")}" as foobar from ${ref("sample_data")} where true
