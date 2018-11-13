9f81c245c7a1eab19be8cb0c7422c9332d725567
Lewis Hemens <lewis@hemens.co.uk>
2018-11-13 18:33:07 +0000

    Add build/run scripts

--- typescript/api/_build.js
+++
@@ -1,6 +0,0 @@
-var { typescript } = require("../_rules.js");
-
-typescript({
-  name: "api",
-  deps: ["//typescript:lerna_bootstrap", "//typescript/protos", "//typescript/core"]
-});
--- typescript/core/_build.js
+++
@@ -1,7 +0,0 @@
-var { typescript } = require("../_rules.js");
-
-typescript({
-  name: "core",
-  build: ctx => [`tsc ${ctx.args}`, `mkdir -p build/parser && cp parser/index.js build/parser/index.js`],
-  deps: ["//typescript:lerna_bootstrap", "//typescript/protos"]
-});
