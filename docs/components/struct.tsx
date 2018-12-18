1220861b995c82881858ed9474226ee9ca82b823
Lewis Hemens <lewis@hemens.co.uk>
2018-12-18 15:28:11 +0000

    Clean up some of the docs build process

---
+++ docs/components/struct.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+export interface Props {
+  description?: string;
+  fields?: {
+    name: string;
+    type: string;
+    typeLink?: string;
+    description: string;
+  }[];
+}
+
+export default class Struct extends React.Component<Props, any> {
+  render() {
+    return (
+      <table className="bp3-html-table bp3-html-table-striped" style={styles.table}>
+        <thead>
+          <tr>
+            <td>Field</td>
+            <td>Type</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.fields.map(field => (
+            <tr key={field.name}>
+              <td>
+                <code>{field.name}</code>
+              </td>
+              <td>
+                <code>{field.type}</code>
+              </td>
+              <td>{field.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export const styles: { [className: string]: React.CSSProperties } = {
+  table: {
+    width: "100%"
+  }
+};
---
+++ docs/pages/_document.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { Navbar, NavbarHeading, NavbarGroup, Button, Alignment } from "@blueprintjs/core";
+
+export default class DefaultDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/static/css/blueprint.css" />
+          <link rel="stylesheet" href="/static/css/normalize.css" />
+          <link rel="stylesheet" href="/static/css/highlight.css" />
+          <link rel="stylesheet" href="/static/css/core.css" />
+          <link rel="shortcut icon" href="/static/images/logo.png" type="image/png" />
+          <script type="text/javascript" src="/static/js/inline.js" />
+        </Head>
+        <body style={styles.body}>
+          <Navbar title="Dataform" fixedToTop className="hideInline">
+            <NavbarGroup>
+              <img src="/static/images/logo_with_text.svg" style={styles.logo} />
+            </NavbarGroup>
+            <NavbarGroup align={Alignment.RIGHT}>
+              <a style={styles.githubLink} href="https://github.com/tada-science/dataform">
+                <img style={styles.githubLogo} src="/static/images/github_logo.png" />
+              </a>
+            </NavbarGroup>
+          </Navbar>
+          <div style={styles.navbarSpacer} className="hideInline" />
+          <div style={styles.contents}>
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+export const styles: { [className: string]: React.CSSProperties } = {
+  body: {
+    margin: "0px",
+    backgroundColor: "#f8f9fa",
+    fontFamily: 'Lato, Roboto, "Helvetica Neue", Arial, sans-serif',
+    color: "#495057",
+    fontSize: "1rem",
+    fontWeight: 400,
+    lineHeight: "1.5"
+  },
+  logo: {
+    height: "100%",
+    opacity: 0.9
+  },
+  contents: {
+    padding: "20px",
+    width: "100%",
+    maxWidth: "960px",
+    margin: "auto"
+  },
+  githubLogo: {
+    filter: "invert(100%)",
+    height: "100%"
+  },
+  githubLink: {
+    height: "100%",
+    padding: "10px"
+  },
+  navbarSpacer: {
+    height: "50px"
+  }
+};
