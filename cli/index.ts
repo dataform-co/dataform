4edc0ec508951ded10d47be0c304e41f93819773
ShevtsovY <yuriy.shevtsov@do-it.co>
2018-12-18 14:53:00 +0200

    added help for cli when run without any args

--- cli/index.ts
+++ cli/index.ts
@@ -69,13 +69,17 @@ yargs
     yargs =>
       yargs
         .positional("project-dir", {
-          describe: "The directory of the Dataform project.",
-          default: "."
+          describe: "The directory of the Dataform project."
         })
         .option("watch", {
-          describe: "Watch the changes in the project directory."
-        }),
+          describe: "Watch the changes in the project directory.",
+          type: "boolean",
+          default: false
+        })
+        .demandOption(['project-dir']),
     argv => {
+      if (!argv["project-dir"]) return;
+
       const projectDir = path.resolve(argv["project-dir"]);
 
       compileProject(projectDir)
@@ -275,4 +279,6 @@ yargs
         .then(results => console.log(JSON.stringify(results, null, 4)))
         .catch(e => console.log(e));
     }
-  ).argv;
+  )
+  .demandCommand(1, 'You need at least one command before moving on')
+  .argv;
