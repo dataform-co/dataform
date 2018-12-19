1eccc1df4a404e396309a6ce9462a1c9107ca85e
Lewis Hemens <lewis@hemens.co.uk>
2018-12-19 17:07:33 +0000

    Move to using tmp fs as a IPC channel, as node IPC doesn't work with graphs > 200kb in size

--- api/vm/compile.ts
+++ api/vm/compile.ts
@@ -3,8 +3,11 @@ import { NodeVM } from "vm2";
 import { compilers } from "@dataform/core";
 import * as protos from "@dataform/protos";
 import { genIndex } from "../gen_index";
+import * as fs from "fs";
+import * as os from "os";
+import * as crypto from "crypto";
 
-export function compile(projectDir: string): protos.ICompiledGraph {
+export function compile(projectDir: string): protos.CompiledGraph {
   const vm = new NodeVM({
     wrapper: "none",
     require: {
@@ -19,13 +22,33 @@ export function compile(projectDir: string): protos.ICompiledGraph {
   return vm.run(indexScript, path.resolve(path.join(projectDir, "index.js")));
 }
 
-process.on(`message`, object => {
+process.on("message", object => {
   try {
-    var graph = compile(object.projectDir);
-    process.send({ result: graph });
-  }
-  catch (e) {
+    // IPC breaks down above 200kb, which is a problem. Instead, pass via file system...
+    // TODO: This isn't ideal.
+    const tmpDir = path.join(os.tmpdir(), "dataform");
+    if (!fs.existsSync(tmpDir)) {
+      fs.mkdirSync(tmpDir);
+    }
+    // Create a consistent hash for the temporary path based on the absolute project path.
+    const absProjectPathHash = crypto
+      .createHash("md5")
+      .update(path.resolve(object.projectDir))
+      .digest("hex");
+    const tmpPath = path.join(tmpDir, absProjectPathHash);
+    // Clear the transfer path before writing it.
+    if (fs.existsSync(tmpPath)) {
+      fs.unlinkSync(tmpPath);
+    }
+    const graph = compile(object.projectDir);
+    // Use protobuffer encoding rather than JSON.
+    const encodedGraph = protos.CompiledGraph.encode(graph).finish();
+    fs.writeFileSync(tmpPath, encodedGraph);
+    // Send back the temp path.
+    process.send({ path: String(tmpPath) });
+  } catch (e) {
+    console.log(e);
     process.send({ err: String(e) });
   }
   process.exit();
-})
+});
