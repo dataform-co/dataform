763deac617149d0d08e8177468ce1a9d682b2c26
Lewis Hemens <lewis@dataform.co>
2019-01-08 11:45:06 +0000

    Change caller file resolution logic to not resolve to files in the includes directory, but only within definitions/ and models/

---
+++ examples/bigquery/definitions/example_deferred.js
@@ -0,0 +1,1 @@
+macros.deferredMaterialize("example_deferred", "select 1 as test");
--- examples/bigquery/includes/macros.js
+++ examples/bigquery/includes/macros.js
@@ -1,3 +1,4 @@
 module.exports = {
   foo: value => `foo_${value}`,
-}
+  deferredMaterialize: (name, arg) => materialize(name, arg)
+};
