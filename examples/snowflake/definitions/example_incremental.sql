d0a887a074c6576a9ef02205e248ac2d7a4d7957
ShevtsovY <yuriy.shevtsov@do-it.co>
2019-01-10 12:51:48 +0200

    fixed snowflake example

--- examples/snowflake/definitions/example_incremental.sql
+++ examples/snowflake/definitions/example_incremental.sql
@@ -1,4 +1,4 @@
---js where(ctx => `ts > (select max(ts) from ${self()}) or (select max(ts) from ${self()}) is null`)
 --js type("incremental");
+--js where(ctx => `ts > (select max(ts) from ${self()}) or (select max(ts) from ${self()}) is null`)
 --js const crossdb = require("@dataform/crossdb");
 select ${crossdb.currentTimestampUTC()} as ${describe("ts")}
