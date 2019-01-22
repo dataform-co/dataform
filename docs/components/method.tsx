9a3e92a6efc91537d1213cdc33f1114cdb357386
ShevtsovY <45006853+ShevtsovY@users.noreply.github.com>
2019-01-22 17:32:20 +0200

    #58: Terminology updates, rename materialization(s) to table(s) and materialize() to publish()

--- docs/components/method.tsx
+++ docs/components/method.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import remark from "remark";
+import remark2react from "remark-react";
 import Struct, { Props as StructProps } from "./struct";
 
 export interface Props extends StructProps {
@@ -8,11 +10,19 @@ export interface Props extends StructProps {
 }
 
 export default class Method extends React.Component<Props, any> {
+  renderDescription = () => {
+    return remark()
+      .use(remark2react)
+      .processSync(this.props.description).contents;
+  };
+
   render() {
     return (
       <div>
-        <h2 id={this.props.name}><pre>{this.props.name}</pre></h2>
-        <div>{this.props.description}</div>
+        <h2 id={this.props.name}>
+          <pre>{this.props.name}</pre>
+        </h2>
+        <div>{this.renderDescription()}</div>
         <div>
           {this.props.signatures &&
             this.props.signatures.map(signature => (
