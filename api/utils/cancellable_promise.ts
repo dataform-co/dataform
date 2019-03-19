2bfc7d174e3458ccadbf5b309a0ae5f9ffd466c8
Lewis Hemens <lewis@dataform.co>
2019-03-19 14:32:44 +0000

    Fix the cancellable promise implementation to not extend Promise directly, which doesn't seem to work properly in some cases. Add unit tests. Fixes dataform-co/dataform-co#236

--- api/utils/cancellable_promise.ts
+++ api/utils/cancellable_promise.ts
@@ -1,23 +1,35 @@
 import { EventEmitter } from "events";
 
-export class CancellablePromise<T> extends Promise<T> {
-    private static CANCEL_EVENT = "cancel";
-  
-    private emitter = new EventEmitter();
-  
-    constructor(
-      method: (
-        resolve: (value?: T | PromiseLike<T>) => void,
-        reject: (reason?: any) => void,
-        onCancel?: (handleCancel: () => void) => void
-      ) => void
-    ) {
-      super((resolve, reject) =>
-        method(resolve, reject, handleCancel => this.emitter.on(CancellablePromise.CANCEL_EVENT, handleCancel))
-      );
-    }
-  
-    public cancel(): void {
-      this.emitter.emit(CancellablePromise.CANCEL_EVENT);
-    }
+export class CancellablePromise<T> implements PromiseLike<T> {
+  private static CANCEL_EVENT = "cancel";
+
+  private emitter = new EventEmitter();
+  private promise: Promise<T>;
+
+  constructor(
+    method: (
+      resolve: (value?: T | PromiseLike<T>) => void,
+      reject: (reason?: any) => void,
+      onCancel?: (handleCancel: () => void) => void
+    ) => void
+  ) {
+    this.promise = new Promise<T>((resolve, reject) =>
+      method(resolve, reject, handleCancel => this.emitter.on(CancellablePromise.CANCEL_EVENT, handleCancel))
+    );
   }
+
+  public then<S>(
+    onfulfilled?: (value: T) => S | PromiseLike<S>,
+    onrejected?: (reason: any) => PromiseLike<never>
+  ): Promise<S> {
+    return this.promise.then(onfulfilled, onrejected);
+  }
+
+  public catch(onRejected?: (reason: any) => PromiseLike<never>): Promise<T> {
+    return this.promise.catch(onRejected);
+  }
+
+  public cancel(): void {
+    this.emitter.emit(CancellablePromise.CANCEL_EVENT);
+  }
+}
