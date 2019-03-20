9e857b237df9bfd06214a9c54e893b8165d03358
Lewis Hemens <lewis@hemens.co.uk>
2019-03-20 18:27:35 +0000

    Fix cancellable promise implementation and add tests (#111)
    * Fixes bigquery db adapter to use both the promise pool and bluebird cancellation properly, adds a related test but doesn't actually test the bigquery adapter properly yet. #dataform-co/dataform-tracking#122
    
    * Set max listeners for the runner event emitter to be infinite
    
    * Make runs and queries cancellable without using bluebird. Change the semantics of cancellation to not throw, but to kill any currently running queries and let dependencies get skipped.
    
    * Address review comments
    
    * Fix missing return
    
    * Fix the cancellable promise implementation to not extend Promise directly, which doesn't seem to work properly in some cases. Add unit tests. Fixes dataform-co/dataform-co#236
    
    * Resolve review comments

---
+++ tests/api/utils/cancellable_promise.spec.ts
@@ -0,0 +1,22 @@
+import { CancellablePromise } from "@dataform/api/utils/cancellable_promise";
+import { expect } from "chai";
+
+describe("cancellable_promise", () => {
+  it("cancel is called", () => {
+    let wasCancelled = false;
+    const promise = new CancellablePromise((resolve, reject, onCancel) => {
+      onCancel(() => {
+        wasCancelled = true;
+      });
+    });
+    promise.cancel();
+    expect(wasCancelled).is.true;
+  });
+
+  it("resolves", async () => {
+    const result = await new CancellablePromise<string>((resolve, reject, onCancel) => {
+      resolve("resolved");
+    });
+    expect(result).equals("resolved");
+  });
+});
