c15148592da701d75bf362d323237d8084cea46a
Ben Birt <benjamin.birt@gmail.com>
2019-03-20 17:02:51 +0000

    Clean up dataform API slightly to make skipping install() consistent with how it's skipped in init().

--- api/commands/install.ts
+++ api/commands/install.ts
@@ -2,6 +2,9 @@ import * as path from "path";
 import * as childProcess from "child_process";
 import { promisify } from "util";
 
-export function install(projectDir: string) {
+export function install(projectDir: string, skipInstall?: boolean) {
+  if (skipInstall) {
+    return;
+  }
   return promisify(childProcess.exec)("npm i", { cwd: path.resolve(projectDir) });
 }
