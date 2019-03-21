ac5407b69bebb5defd3523fb94c040b0530c3763
Ben Birt <benjamin.birt@gmail.com>
2019-03-21 08:55:10 +0000

    Lewis comments

--- api/commands/install.ts
+++ api/commands/install.ts
@@ -2,9 +2,9 @@ import * as path from "path";
 import * as childProcess from "child_process";
 import { promisify } from "util";
 
-export function install(projectDir: string, skipInstall?: boolean) {
+export async function install(projectDir: string, skipInstall?: boolean) {
   if (skipInstall) {
     return;
   }
-  return promisify(childProcess.exec)("npm i", { cwd: path.resolve(projectDir) });
+  await promisify(childProcess.exec)("npm i", { cwd: path.resolve(projectDir) });
 }
