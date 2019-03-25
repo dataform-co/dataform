738ed48e74b67cf5b3d06558666a198e14a82685
Lewis Hemens <lewis@hemens.co.uk>
2019-03-25 11:18:45 +0000

    Add tslint config and fix prettier config to match other repository and apply bulk fixes across the whole codebase. (#117)

--- api/commands/table.ts
+++ api/commands/table.ts
@@ -5,6 +5,9 @@ export function list(profile: protos.IProfile): Promise<protos.ITarget[]> {
   return dbadapters.create(profile).tables();
 }
 
-export function get(profile: protos.IProfile, target: protos.ITarget): Promise<protos.ITableMetadata> {
+export function get(
+  profile: protos.IProfile,
+  target: protos.ITarget
+): Promise<protos.ITableMetadata> {
   return dbadapters.create(profile).table(target);
 }
--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
+import { build, Builder } from "./commands/build";
 import { compile } from "./commands/compile";
 import { init } from "./commands/init";
 import { install } from "./commands/install";
 import * as query from "./commands/query";
 import { run, Runner } from "./commands/run";
-import { build, Builder } from "./commands/build";
 import * as table from "./commands/table";
 import * as utils from "./utils";
 
