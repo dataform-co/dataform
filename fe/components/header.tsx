d6ad2dffc9ceb15bd97d3f057a3bc56bfd054db7
Lewis Hemens <lewis@dataform.co>
2019-03-28 11:00:27 +0000

    Stash

--- fe/components/header.tsx
+++ fe/components/header.tsx
@@ -4,36 +4,37 @@ import * as styles from "df/fe/components/header.css";
 
 import { Button, Intent } from "@blueprintjs/core";
 
+import MenuCloseIcon from "df/static/images/menu_close.svg";
+import MenuIcon from "df/static/images/menu_icon.svg";
+import MenuIconWhite from "df/static/images/menu_icon_white.svg";
+import LogoImage from "df/static/images/new_logo_with_text.svg";
+import LogoImageWhite from "df/static/images/new_logo_with_text_white.svg";
+
 export interface IPage {
-  path?: string;
   name: string;
+  path?: string;
 }
 
 interface IProps {
-  pages: Array<IPage>;
-  pagePath?: string;
+  pages?: IPage[];
+  currentPath?: string;
   invert?: boolean;
   maxWidth?: string;
 }
 
 interface IState {
-  scroll?: number;
+  showFixedNav?: boolean;
   showMobileMenu?: boolean;
 }
-
-const getPath = (p: IPage) => p.path || p.name.replace(" ", "_");
-
-const DEFAULT_MAX_WIDTH = "1200px";
 
 export class Header extends React.Component<IProps, IState> {
   public state: IState = {};
 
   public handleScroll = e => {
-    this.setState({ scroll: window.scrollY });
+    this.setState({ showFixedNav: window.scrollY > 100 });
   };
 
   public componentDidMount() {
-    this.handleScroll(null);
     window.addEventListener("scroll", this.handleScroll);
   }
 
@@ -47,38 +48,38 @@ export class Header extends React.Component<IProps, IState> {
 
   public render() {
     const navClasses = [styles.nav];
-    if (this.state.scroll > 100) {
+    const { showFixedNav } = this.state;
+    const { invert } = this.props;
+
+    if (showFixedNav) {
       navClasses.push(styles.navScrolled);
-    } else if (this.props.invert) {
+    } else if (invert) {
       navClasses.push(styles.navInverted);
     }
+    const maxWidth = this.props.maxWidth || "1200px";
     return (
       <div>
         <div className={styles.hideMobile}>
           <nav className={navClasses.join(" ")}>
-            <span
-              className={styles.navContent}
-              style={{ maxWidth: this.props.maxWidth || DEFAULT_MAX_WIDTH }}
-            >
+            <span className={styles.navContent} style={{ maxWidth }}>
               <span>
-                <a href="/">
-                  <img className={styles.logoImage} />
-                </a>
+                <a href="/">{this.createColouredLogo(invert, showFixedNav)}</a>
               </span>
               <span className={styles.pages}>
-                {this.props.pages.map(p => (
+                {this.props.pages.map(page => (
                   <span
-                    id={p.name}
-                    className={this.props.pagePath === getPath(p) ? styles.pageSelected : ""}
+                    key={page.name}
+                    id={page.name}
+                    className={this.props.currentPath === page.path ? styles.pageSelected : ""}
                   >
-                    <a href={`/${getPath(p)}`}>{p.name}</a>
+                    <a href={page.path}>{page.name}</a>
                   </span>
                 ))}
               </span>
               <span className={styles.actions}>
                 <span>
                   <a href="https://app.eu.dataform.co">
-                    <Button text="Log in" intent={Intent.PRIMARY} />
+                    <Button text="Log in" intent={Intent.PRIMARY} className={"ghost"} />
                   </a>
                 </span>
                 <span>
@@ -95,14 +96,12 @@ export class Header extends React.Component<IProps, IState> {
             <nav className={navClasses.join(" ")}>
               <span className={styles.navContent}>
                 <span>
-                  <a href="/">
-                    <img className={styles.logoImage} />
-                  </a>
+                  <a href="/">{this.createColouredLogo(invert, showFixedNav)}</a>
                 </span>
               </span>
               <span className={styles.actions}>
                 <span onClick={this.handleMobileMenuChange}>
-                  <img className={styles.menuIcon} />
+                  {this.createColouredMenuLogo(invert, showFixedNav)}
                 </span>
               </span>
             </nav>
@@ -113,24 +112,26 @@ export class Header extends React.Component<IProps, IState> {
                 <span className={styles.navContent}>
                   <span>
                     <a href="/">
-                      <img className={styles.logoImageWhite} />
+                      <img src={LogoImageWhite} className={styles.logoImageWhite} />
                     </a>
                   </span>
                 </span>
                 <span className={styles.actions}>
                   <span onClick={this.handleMobileMenuChange}>
-                    <img className={styles.menuCloseIcon} />
+                    <img src={MenuCloseIcon} className={styles.menuCloseIcon} />
                   </span>
                 </span>
               </nav>
               <div className={styles.mobileMenu}>
-                {this.props.pages.map(p => (
-                  <>
+                {this.props.pages.map(page => (
+                  <React.Fragment key={page.path}>
                     <div className={styles.mobileMenuDivider} />
-                    <a href={`/${getPath(p)}`} key={getPath(p)}>
-                      <div className={styles.mobileMenuItem}>{p.name}</div>
+                    <a href={page.path}>
+                      <div className={styles.mobileMenuItem} id={page.name}>
+                        {page.name}
+                      </div>
                     </a>
-                  </>
+                  </React.Fragment>
                 ))}
               </div>
               <div className={styles.mobileMenuActions}>
@@ -151,4 +152,17 @@ export class Header extends React.Component<IProps, IState> {
       </div>
     );
   }
+
+  // TODO just use the fill property in the svg to do this
+  private createColouredLogo = (invert: boolean, showFixedNav: boolean) => {
+    return invert && !showFixedNav ? (
+      <img src={LogoImageWhite} className={styles.logoImage} />
+    ) : (
+      <img src={LogoImage} className={styles.logoImage} />
+    );
+  };
+
+  private createColouredMenuLogo = (invert: boolean, showFixedNav: boolean) => {
+    return invert && !showFixedNav ? <img src={MenuIconWhite} /> : <img src={MenuIcon} />;
+  };
 }
