0dbba7431194eafa73334c9fb55252da7e118609
ghh <ghhuon@gmail.com>
2019-04-15 18:59:11 +0100

    small css cleanup

--- docs/layouts/base.tsx
+++ docs/layouts/base.tsx
@@ -13,8 +13,7 @@ export interface IProps {
   title: string;
 }
 
-const PAGES = [
-];
+const PAGES = [];
 
 const MAX_WIDTH = "1500px";
 
@@ -29,7 +28,6 @@ export class BaseLayout extends React.Component<IProps, {}> {
         <div className={styles.container} style={{ maxWidth: MAX_WIDTH }}>
           {this.props.children}
         </div>
-        <Footer maxWidth={MAX_WIDTH} />
       </div>
     );
   }
---
+++ testgh/definitions/second_model.sql
@@ -0,0 +1,1 @@
+SELECT one FROM ${ref("first_model")}
