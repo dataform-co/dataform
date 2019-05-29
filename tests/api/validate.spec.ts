97d3d8bc746956c93cb42b6882f84670f29e8f5b
Ahmad Faiyaz <faiyaz26@gmail.com>
2019-05-29 15:29:17 +0100

    Add schedule initialization and validation (#223)
    * Add schedule initialization and validation
    
    * resolve comments
    
    * create empty file on schedules.json
    
    * fix
    
    * resolve pr comments
    
    * add action validation
    
    * fix2
    
    * add comment
    
    * fix
    
    * fix 3
    
    * f

---
+++ tests/api/validate.spec.ts
@@ -0,0 +1,95 @@
+import { validateSchedules } from "@dataform/api";
+import { dataform } from "@dataform/protos";
+import { assert, config, expect } from "chai";
+
+describe("@dataform/api/validate", () => {
+  describe("validateSchedules", () => {
+
+    it("returns no errors for valid schedules object", () => {
+      const compiledGraph = dataform.CompiledGraph.create({
+        tables: [
+          {
+            name: "action1"
+          },
+          {
+            name: "action2"
+          }
+        ]
+      });
+      const valid_schedule = dataform.schedules.SchedulesJSON.create({
+        defaultNotification: {
+          emails: ["team@dataform.co", "abc@test.com"],
+          onSuccess: true,
+          onFailure: true
+        },
+        schedules: [
+          {
+            name: "name1",
+            cron: "*/2 * * * *",
+            disabled: false,
+            options: {
+              actions: ["action2"]
+            }
+          },
+          {
+            name: "name2",
+            cron: "*/2 * * * *",
+            notification: {
+              emails: ["tada@test.com"],
+              onSuccess: true,
+              onFailure: false
+            }
+          }
+        ]
+      })
+
+      const errors = validateSchedules(valid_schedule, compiledGraph);
+      expect(errors).to.eql([]);
+    });
+
+    it("test all errors", () => {
+      const compiledGraph = dataform.CompiledGraph.create({
+        tables: [
+          {
+            name: "action1"
+          },
+          {
+            name: "action2"
+          }
+        ]
+      });
+
+      const invalid_schedule = dataform.schedules.SchedulesJSON.create({
+        defaultNotification: {
+          emails: ["test.com"]
+        },
+        schedules: [
+          {
+            name: "name1",
+            cron: "asdas",
+            notification: {
+              emails: ["test2.com"]
+            },
+            options: {
+              actions: ["action3"]
+            }
+          },
+          {
+            name: "name1",
+            cron: "*/2 * * * *"
+          }
+        ]
+      });
+
+      const errors = validateSchedules(invalid_schedule, compiledGraph);
+      const expectedErrors = [
+        '"test.com" is not a valid email address.',
+        '"asdas" is not a valid cron expression.',
+        '"action3" doesn\'t exist in the project.',
+        '"test2.com" is not a valid email address.',
+        '"name1" is not unique. All schedule names must be unique.'
+      ];
+      expect(errors).to.eql(expectedErrors);
+    });
+  });
+});
