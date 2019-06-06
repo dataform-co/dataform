d8fe59b6b0f4c390f80ea06acc8b3acb171f4c94
BenBirt <benjamin.birt@gmail.com>
2019-06-06 09:18:38 +0100

    Revert "Remove axios vulnerability. Make it possible to use this code locally in a dataform-co build again. (#248)" (#250)
    This reverts commit 8027be98a044ad93bb3cff1507f7b08802eb4f6d.

--- tools/json-merge/index.ts
+++ tools/json-merge/index.ts
@@ -4,11 +4,11 @@ import * as yargs from "yargs";
 const argv = yargs.option("output-path", { required: true }).option("layer-paths", { array: true })
   .argv;
 
-const result = (argv.layerPaths as string[])
+const result = argv.layerPaths
   .map((layerPath: string) => JSON.parse(fs.readFileSync(layerPath, "utf8")))
   .reduce(
     (layerJson: object, accumulatorJson: object) => ({ ...accumulatorJson, ...layerJson }),
     {}
   );
 
-fs.writeFileSync(argv.outputPath as string, JSON.stringify(result, null, 4));
+fs.writeFileSync(argv.outputPath, JSON.stringify(result, null, 4));
