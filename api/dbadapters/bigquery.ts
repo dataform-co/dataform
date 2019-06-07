024902fb8073684c8645b26ce0c691e5bb92f79b
Ben Birt <benjamin.birt@gmail.com>
2019-06-07 15:55:49 +0100

    fix prepareSchema

--- api/dbadapters/bigquery.ts
+++ api/dbadapters/bigquery.ts
@@ -120,28 +120,25 @@ export class BigQueryDbAdapter implements DbAdapter {
       });
   }
 
-  public prepareSchema(schema: string): Promise<void> {
+  public async prepareSchema(schema: string): Promise<void> {
     const location = this.bigQueryCredentials.location || "US";
 
-    // If metadata call fails, it probably doesn't exist. So try to create it.
-    return this.client
-      .dataset(schema)
-      .getMetadata()
-      .then(
-        data => {
-          const metadata = data[0];
-          // check location of the dataset
-          const wrongMetadata = metadata.location.toUpperCase() !== location.toUpperCase();
-          if (wrongMetadata) {
-            const message = `Cannot create dataset "${schema}" in location "${location}" as it already exists in location "${
-              metadata.location
-            }". Change your default dataset location or delete the existing dataset.`;
-            throw Error(message);
-          }
-        },
-        _ => this.client.createDataset(schema, { location }).then(() => {})
-      )
-      .catch(e => console.error(e));
+    let metadata;
+    try {
+      const data = this.client.dataset(schema).getMetadata();
+      metadata = data[0];
+    } catch (e) {
+      // If metadata call fails, it probably doesn't exist. So try to create it.
+      return this.client.createDataset(schema, { location });
+    }
+
+    if (metadata.location.toUpperCase() !== location.toUpperCase()) {
+      throw new Error(
+        `Cannot create dataset "${schema}" in location "${location}". It already exists in location "${
+          metadata.location
+        }". Change your default dataset location or delete the existing dataset.`
+      );
+    }
   }
 }
 
