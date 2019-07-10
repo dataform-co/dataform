bef4ce4be8c36e7fc6d129e5cdb96017596c40e9
Ben Birt <benjamin.birt@gmail.com>
2019-07-10 12:28:12 +0100

    more complete test cases

--- tests/integration/redshift_project/definitions/test.js
+++ tests/integration/redshift_project/definitions/test.js
@@ -3,15 +3,15 @@ test("test case")
   .input(
     "sample_data",
     `
-        select 'hi' as faked union all
-        select 'ben' as faked union all
-        select 'sup?' as faked
+        select 'hi' as col1, 1 as col2, 3.5 as col3, true as col4 union all
+        select 'ben' as col2, 2 as col2, 6.5 as col3, false as col4 union all
+        select 'sup?' as col3, 3 as col2, 9.5 as col3, true as col4
     `
   )
   .expect(
     `
-        select 'hi' as faked union all
-        select 'ben' as faked union all
-        select 'sup?' as faked
+        select 'hi' as col1, 1 as col2, 3.5 as col3, true as col4 union all
+        select 'ben' as col2, 2 as col2, 6.5 as col3, false as col4 union all
+        select 'sup?' as col3, 3 as col2, 9.5 as col3, true as col4
     `
   );
