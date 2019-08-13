6cc53eb5e1c358ec75eccf115d9b580f9ba61b8a
Ben Birt <benjamin.birt@gmail.com>
2019-08-13 15:38:06 +0100

    Make sqldatawarehouse.spec.ts work

--- tests/integration/sqldatawarehouse.spec.ts
+++ tests/integration/sqldatawarehouse.spec.ts
@@ -62,26 +62,28 @@ describe("@dataform/integration/sqldatawarehouse", () => {
     const actionMap = keyBy(executedGraph.actions, v => v.name);
 
     // Check the status of the two assertions.
-    expect(actionMap["df_integration_test.example_assertion_fail"].status).equals(
+    expect(actionMap["df_integration_test_assertions.example_assertion_fail"].status).equals(
       dataform.ActionExecutionStatus.FAILED
     );
-    expect(actionMap["df_integration_test.example_assertion_pass"].status).equals(
+    expect(actionMap["df_integration_test_assertions.example_assertion_pass"].status).equals(
       dataform.ActionExecutionStatus.SUCCESSFUL
     );
 
     // Check the status of the two uniqueness assertions.
-    expect(actionMap["df_integration_test.example_assertion_uniqueness_fail"].status).equals(
-      dataform.ActionExecutionStatus.FAILED
-    );
     expect(
-      actionMap["df_integration_test.example_assertion_uniqueness_fail"].tasks[2].error
+      actionMap["df_integration_test_assertions.example_assertion_uniqueness_fail"].status
+    ).equals(dataform.ActionExecutionStatus.FAILED);
+    expect(
+      actionMap["df_integration_test_assertions.example_assertion_uniqueness_fail"].tasks[2].error
     ).to.eql("Assertion failed: query returned 1 row(s).");
-    expect(actionMap["df_integration_test.example_assertion_uniqueness_pass"].status).equals(
-      dataform.ActionExecutionStatus.SUCCESSFUL
-    );
+    expect(
+      actionMap["df_integration_test_assertions.example_assertion_uniqueness_pass"].status
+    ).equals(dataform.ActionExecutionStatus.SUCCESSFUL);
 
     // Check the data in the incremental table.
-    let incrementalTable = keyBy(compiledGraph.tables, t => t.name).example_incremental;
+    const incrementalTable = keyBy(compiledGraph.tables, t => t.name)[
+      "df_integration_test.example_incremental"
+    ];
     let incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
     expect(incrementalRows.length).equals(1);
 
@@ -96,7 +98,6 @@ describe("@dataform/integration/sqldatawarehouse", () => {
     expect(executedGraph.ok).equals(true);
 
     // Check there is an extra row in the incremental table.
-    incrementalTable = keyBy(compiledGraph.tables, t => t.name).example_incremental;
     incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
     expect(incrementalRows.length).equals(2);
   }).timeout(60000);
