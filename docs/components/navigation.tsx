8c97a274b2bd65b949b0e1a059b5b930e45c8018
DanLee <dan@dataform.co>
2019-08-29 17:43:57 +0100

    Adding docs for tags (#402)
    * tags docs
    
    * added tags
    
    * Update navigation.tsx
    
    * Rename tags to tags.mdx
    
    * Update docs/pages/guides/tags.mdx
    
    Co-Authored-By: BenBirt <benjamin.birt@gmail.com>
    
    * Update tags.mdx
    
    added link to schedules page
    
    * Update tags.mdx
    
    fixed link

--- docs/components/navigation.tsx
+++ docs/components/navigation.tsx
@@ -17,6 +17,7 @@ import * as guideIncrementalDatasets from "df/docs/pages/guides/incremental-data
 import * as guideInlineTables from "df/docs/pages/guides/inline-tables.mdx";
 import * as guideJsApi from "df/docs/pages/guides/js-api.mdx";
 import * as guideOperations from "df/docs/pages/guides/operations.mdx";
+import * as guideTags from "df/docs/pages/guides/tags.mdx";
 import * as guideTests from "df/docs/pages/guides/tests.mdx";
 
 // Framework warehouses.
@@ -57,6 +58,7 @@ export default class Navigation extends React.Component<IProps> {
             guideAssertions,
             guideDocumentation,
             guideTests,
+            guideTags,
             guideConfiguration,
             guideBuiltInFunctions,
             guideJsApi,
