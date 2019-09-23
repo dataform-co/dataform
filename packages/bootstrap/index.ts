e4cb732a379851adc6d201b47b1b25a26ded7e23
Lewis Hemens <lewis@hemens.co.uk>
2019-09-23 08:52:41 +0100

    Start trying to get reflect-metadata to work for generating UDF boilerplate

--- packages/bootstrap/index.ts
+++ packages/bootstrap/index.ts
@@ -1,4 +1,5 @@
 import alea from "alea";
+import "reflect-metadata";
 
 interface IBootstrapOptions {
   iterations: number;
@@ -18,57 +19,65 @@ const DEFAULT_OPTIONS: IBootstrapOptions = {
   confidenceInterval: 0.95
 };
 
-export const reducers = {
-  sum: (values: number[]) => values.reduce((acc, curr) => acc + curr, 0),
-  mean: (values: number[]) => reducers.sum(values) / values.length
-};
-
-export function estimate<T>(
-  values: T[],
-  reducer: (values: T[]) => number,
-  optionOverrides?: Partial<IBootstrapOptions>
-): IEstimate {
-  const options = { ...DEFAULT_OPTIONS, ...optionOverrides };
-  const estimates: number[] = [];
-  const rng = alea(options.seed);
-  for (let i = 0; i < options.iterations; i++) {
-    estimates.push(reducer(sample(values, rng)));
+export class Reducers {
+  public static sum() {
+    return (values: number[]) => values.reduce((acc, curr) => acc + curr, 0);
   }
-  // Sort the estimates. Make sure these get ordered as numbers, not strings... #javascript
-  estimates.sort((a, b) => +a - +b);
-
-  // Compute the top and bottom fractiles we are looking for from the interval.
-  const remainderIntervalSize = 1 - options.confidenceInterval;
-  const lowerFractile = remainderIntervalSize / 2;
-  const upperFractile = 1 - lowerFractile;
-  return {
-    lower: interpolatedFractile(estimates, lowerFractile),
-    value: reducer(values),
-    upper: interpolatedFractile(estimates, upperFractile)
-  };
+  public static mean() {
+    return (values: number[]) => Reducers.sum()(values) / values.length;
+  }
 }
 
-function sample<T>(values: T[], rng: () => number): T[] {
-  const result: T[] = [];
-  // tslint:disable-next-line: prefer-for-of
-  for (let i = 0; i < values.length; i++) {
-    result.push(values[Math.floor(rng() * values.length)]);
-  }
-  return result;
-}
+export class Bootstrap {
 
-function interpolatedFractile(values: number[], fractile: number) {
-  // We treat the last value as the fractile 1.0 otherwise it won't be symmetric, hence the -1.
-  // With 1000 values, this maps the fractile range (0.0, 1.0) to the indices range (0, 999).
-  const targetIndex = fractile * (values.length - 1);
-  const floorIndex = Math.floor(targetIndex);
-  const ceilIndex = Math.ceil(targetIndex);
-  const floorValue = values[floorIndex];
-  const ceilValue = values[ceilIndex];
-  if (floorIndex === ceilIndex) {
-    return floorValue;
+  @Reflect.metadata("udf", true)
+  public static estimate<T>(
+    values: T[],
+    reducer: (values: T[]) => number,
+    optionOverrides?: Partial<IBootstrapOptions>
+  ): IEstimate {
+    const options = { ...DEFAULT_OPTIONS, ...optionOverrides };
+    const estimates: number[] = [];
+    const rng = alea(options.seed);
+    for (let i = 0; i < options.iterations; i++) {
+      estimates.push(reducer(Bootstrap.sample(values, rng)));
+    }
+    // Sort the estimates. Make sure these get ordered as numbers, not strings... #javascript
+    estimates.sort((a, b) => +a - +b);
+
+    // Compute the top and bottom fractiles we are looking for from the interval.
+    const remainderIntervalSize = 1 - options.confidenceInterval;
+    const lowerFractile = remainderIntervalSize / 2;
+    const upperFractile = 1 - lowerFractile;
+    return {
+      lower: Bootstrap.interpolatedFractile(estimates, lowerFractile),
+      value: reducer(values),
+      upper: Bootstrap.interpolatedFractile(estimates, upperFractile)
+    };
   }
-  const indexInterval = targetIndex - floorIndex;
-  // If the value we actually want is between two values, we linearly interpolate between them.
-  return indexInterval * floorValue + (1 - indexInterval) * ceilValue;
+
+  public static sample<T>(values: T[], rng: () => number): T[] {
+    const result: T[] = [];
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < values.length; i++) {
+      result.push(values[Math.floor(rng() * values.length)]);
+    }
+    return result;
+  }
+
+  public static interpolatedFractile(values: number[], fractile: number) {
+    // We treat the last value as the fractile 1.0 otherwise it won't be symmetric, hence the -1.
+    // With 1000 values, this maps the fractile range (0.0, 1.0) to the indices range (0, 999).
+    const targetIndex = fractile * (values.length - 1);
+    const floorIndex = Math.floor(targetIndex);
+    const ceilIndex = Math.ceil(targetIndex);
+    const floorValue = values[floorIndex];
+    const ceilValue = values[ceilIndex];
+    if (floorIndex === ceilIndex) {
+      return floorValue;
+    }
+    const indexInterval = targetIndex - floorIndex;
+    // If the value we actually want is between two values, we linearly interpolate between them.
+    return indexInterval * floorValue + (1 - indexInterval) * ceilValue;
+  }
 }
