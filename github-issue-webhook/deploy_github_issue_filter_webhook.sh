404fff8a9bab0d1c9ad8d84678c816ca63876ae9
Ben Birt <benjamin.birt@gmail.com>
2019-10-19 19:39:43 +0100

    lots of cleanup

--- github-issue-webhook/deploy_github_issue_filter_webhook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bazel build github-issue-webhook
-bazel build github-issue-webhook:package
-
-gcloud functions deploy handleStackdriverEvent \
-  --source=bazel-bin/github-issue-webhook \
-  --runtime nodejs10 \
-  --trigger-http
