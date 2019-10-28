821b57efbb433e24728ad352e95833d85038ba67
BenBirt <benjamin.birt@gmail.com>
2019-10-28 16:23:10 +0000

    Bazelify the cloud function which handles the open-source build status image. (#473)

--- readme/badges/deploy_cloudbuild_badge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gsutil mb -p tada-analytics gs://dataform-cloud-build-badges/
-
-gsutil cp -r readme/badges/build gs://dataform-cloud-build-badges/
-
-gsutil acl set public-read gs://dataform-cloud-build-badges/build/status.svg
-
-gcloud functions deploy status --source=readme/badges/cloudfunction --runtime nodejs10 --trigger-resource cloud-builds --trigger-event google.pubsub.topic.publish
