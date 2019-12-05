667b230ccdfebb2afe880072a3225204e02e08ee
Lewis Hemens <lewis@dataform.co>
2019-12-05 10:22:55 +0000

    Only query for spectrum tables when they exist (#506)

--- api/dbadapters/redshift.ts
+++ api/dbadapters/redshift.ts
@@ -37,13 +37,18 @@ export class RedshiftDbAdapter implements IDbAdapter {
   }
 
   public async tables(): Promise<dataform.ITarget[]> {
+    const hasSpectrumTables = await this.hasSpectrumTables();
     const rows = await this.execute(
       `select table_name, table_schema
      from information_schema.tables
      where table_schema != 'information_schema'
        and table_schema != 'pg_catalog'
        and table_schema != 'pg_internal'
-       union select tablename as table_name, schemaname as table_schema from svv_external_tables`,
+       ${
+         hasSpectrumTables
+           ? "union select tablename as table_name, schemaname as table_schema from svv_external_tables"
+           : ""
+       }`,
       { maxResults: 10000 }
     );
     return rows.map(row => ({
@@ -53,22 +58,29 @@ export class RedshiftDbAdapter implements IDbAdapter {
   }
 
   public async table(target: dataform.ITarget): Promise<dataform.ITableMetadata> {
+    const hasSpectrumTables = await this.hasSpectrumTables();
     const [columnResults, tableResults, externalTableResults] = await Promise.all([
       this.execute(
         `select column_name, data_type, is_nullable
        from information_schema.columns
        where table_schema = '${target.schema}' and table_name = '${target.name}'
-       union
+       ${
+         hasSpectrumTables
+           ? `union
        select columnname as column_name, external_type as data_type, 'not_available' as is_nullable 
        from svv_external_columns 
        where schemaname = '${target.schema}' and tablename = '${target.name}'`
+           : ""
+       }`
       ),
       this.execute(
         `select table_type from information_schema.tables where table_schema = '${target.schema}' and table_name = '${target.name}'`
       ),
-      this.execute(
-        `select 'TABLE' as table_type from svv_external_tables where schemaname = '${target.schema}' and tablename = '${target.name}'`
-      )
+      hasSpectrumTables
+        ? this.execute(
+            `select 'TABLE' as table_type from svv_external_tables where schemaname = '${target.schema}' and tablename = '${target.name}'`
+          )
+        : []
     ]);
     const allTableResults = tableResults.concat(externalTableResults);
     if (allTableResults.length > 0) {
@@ -97,6 +109,18 @@ export class RedshiftDbAdapter implements IDbAdapter {
 
   public async close() {
     await this.queryExecutor.close();
+  }
+
+  private async hasSpectrumTables() {
+    return (
+      (await this.execute(
+        `select 1
+         from information_schema.tables
+         where table_name = 'svv_external_tables'
+           and table_schema = 'pg_catalog'`,
+        { maxResults: 1 }
+      )).length > 0
+    );
   }
 }
 
