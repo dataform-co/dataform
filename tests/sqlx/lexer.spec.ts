95f560df1a4d894d777292bcc36bd46c30f1b6fa
Elias Kassell <elias@dataform.co>
2019-12-17 10:04:29 +0000

    Removed underscores from test names, added deep equals comparison

--- tests/sqlx/lexer.spec.ts
+++ tests/sqlx/lexer.spec.ts
@@ -3,20 +3,18 @@ import { expect } from "chai";
 
 describe("@dataform/sqlx", () => {
   describe("lexer", () => {
-    it("replaces_single_backslash_with_double_backslashes", () => {
+    it("replaces single backslash with double backslashes", () => {
       // These look like groups of 2 backslashes, but are actually 1. If someone
       // in SQLX SQL writes "\" in the regex, it should be interpreted as "\".
       const parsedSqlx = parseSqlx("SELECT regexp_extract('01a_data_engine', '^(\\d{2}\\w)')");
-      expect(parsedSqlx.sql[0]).equals(
-        "SELECT regexp_extract('01a_data_engine', '^(\\\\d{2}\\\\w)')"
-      );
+      expect(parsedSqlx.sql).eql(["SELECT regexp_extract('01a_data_engine', '^(\\\\d{2}\\\\w)')"]);
     });
-    it("replaces_double_backslashes_with_quadruple_backslashes", () => {
+    it("replaces double backslashes with quadruple backslashes", () => {
       // In SQLX SQL, "\\" should be interpreted as "\\".
       const parsedSqlx = parseSqlx("SELECT regexp_extract('01a_data_engine', '^(\\\\d{2}\\\\w)')");
-      expect(parsedSqlx.sql[0]).equals(
+      expect(parsedSqlx.sql).eql([
         "SELECT regexp_extract('01a_data_engine', '^(\\\\\\\\d{2}\\\\\\\\w)')"
-      );
+      ]);
     });
   });
 });
