4823f1d5e3e2ca6c1c2532fcb7876e6dfb950610
Elias Kassell <elias@dataform.co>
2019-12-24 14:23:48 +0000

    Removed link from parents in nav

--- docs/components/navigation.tsx
+++ docs/components/navigation.tsx
@@ -14,6 +14,7 @@ export default class Navigation extends React.Component<IProps> {
     return <div className={styles.navigation}>{this.renderTrees(this.props.tree.children)}</div>;
   }
   private renderTrees(trees: IFileTree[], depth = 0) {
+    console.log("THE TREE:", trees);
     return (
       <ul className={styles[`depth${depth}`]}>
         {trees.map(tree => {
@@ -25,16 +26,26 @@ export default class Navigation extends React.Component<IProps> {
           if (hasChildren) {
             classNames.push(styles.hasChildren);
           }
+
+          let navItem;
+          if (hasChildren && depth > 0) {
+            navItem = (
+              <div>
+                {tree.attributes.title}
+                <Icon icon="chevron-right" />
+              </div>
+            );
+          } else {
+            navItem = (
+              <a href={`/${this.props.version ? `v/${this.props.version}/` : ""}${tree.file.path}`}>
+                {tree.attributes.title}
+              </a>
+            );
+          }
+
           return (
             <React.Fragment key={tree.file.path}>
-              <li className={classNames.join(" ")}>
-                <a
-                  href={`/${this.props.version ? `v/${this.props.version}/` : ""}${tree.file.path}`}
-                >
-                  {tree.attributes.title}
-                  {hasChildren && depth > 0 && <Icon icon="chevron-right" />}
-                </a>
-              </li>
+              <li className={classNames.join(" ")}>{navItem}</li>
               {tree.children &&
                 tree.children.length > 0 &&
                 this.renderTrees(tree.children, depth + 1)}
