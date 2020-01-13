335a9a99562d807135145eef3eea9f678a02201a
Lewis Hemens <lewis@hemens.co.uk>
2020-01-13 08:18:38 +0000

    Core cesting libs

---
+++ testing/hook.ts
@@ -0,0 +1,69 @@
+import { IRunContext, IRunResult } from "df/testing";
+
+export type IHookFunction = () => any;
+
+export interface IHookOptions {
+  name: string;
+  timeout?: number;
+}
+
+export type IHookHandler = (
+  nameOrOptions: IHookOptions | string,
+  optionsOrFn: Omit<IHookOptions, "name"> | IHookFunction,
+  fn?: IHookFunction
+) => void;
+
+export class Hook {
+  public static readonly DEFAULT_TIMEOUT = 30000;
+
+  public static create(
+    nameOrOptions: IHookOptions | string,
+    optionsOrFn: Omit<IHookOptions, "name"> | IHookFunction,
+    fn?: IHookFunction
+  ) {
+    let options: IHookOptions = { name: null };
+    if (typeof nameOrOptions === "string") {
+      options.name = nameOrOptions;
+    } else {
+      options = { ...nameOrOptions };
+    }
+    if (typeof optionsOrFn === "function") {
+      fn = optionsOrFn;
+    } else {
+      options = { ...options, ...optionsOrFn };
+    }
+    return new Hook(options, fn);
+  }
+
+  constructor(public readonly options: IHookOptions, private readonly fn: IHookFunction) {}
+
+  public async run(ctx: IRunContext) {
+    let timer: NodeJS.Timer;
+    const timeout = this.options.timeout || Hook.DEFAULT_TIMEOUT;
+    const result: Partial<IRunResult> = {
+      path: [...ctx.path, `${this.options.name} (hook)`]
+    };
+    try {
+      await Promise.race([
+        this.fn(),
+        new Promise((_, reject) => {
+          timer = setTimeout(() => {
+            result.outcome = "timeout";
+            reject(new Error(`Timed out (${timeout}ms).`));
+          }, timeout);
+        })
+      ]);
+      result.outcome = "passed";
+    } catch (e) {
+      if (result.outcome !== "timeout") {
+        result.outcome = "failed";
+      }
+      result.err = e;
+      ctx.results.push(result as IRunResult);
+      // If hooks fail, we throw anyway.
+      throw e;
+    }
+
+    clearTimeout(timer);
+  }
+}
---
+++ testing/runner.ts
@@ -0,0 +1,114 @@
+import chalk from "chalk";
+import { Suite } from "df/testing";
+import { promisify } from "util";
+
+export interface IRunResult {
+  path: string[];
+  err: any;
+  outcome: "passed" | "timeout" | "failed";
+}
+
+export interface IRunContext {
+  path: string[];
+  results: IRunResult[];
+}
+
+export class Runner {
+  public static setNoExit(noExit: boolean) {
+    Runner.noExit = noExit;
+  }
+  public static queueRunAndExit() {
+    if (!Runner.resultPromise) {
+      Runner.resultPromise = Runner.run();
+    }
+  }
+
+  // tslint:disable: no-console
+  public static async run() {
+    // We tell the runner to start running at the end of current block of
+    // synchronously executed code. This will typically be after all the
+    // suite definitions are evaluated.
+    await promisify(process.nextTick)();
+    const ctx: IRunContext = {
+      path: [],
+      results: []
+    };
+
+    await Suite.globalStack[0].run(ctx);
+
+    if (ctx.results.length === 0) {
+      ctx.results.push({
+        path: [],
+        err: new Error("No tests found in top level test suite."),
+        outcome: "failed"
+      });
+    }
+    const indent = (value: string, levels = 4) =>
+      value
+        .split("\n")
+        .map(line => `${new Array(levels).fill(" ").join("")}${line}`)
+        .join("\n");
+
+    for (const result of ctx.results) {
+      const outcomeString = (result.outcome || "unknown").toUpperCase();
+      const pathString = result.path.join(" > ");
+
+      const colorFn =
+        result.outcome === "failed" || result.outcome === "timeout"
+          ? chalk.red
+          : result.outcome === "passed"
+          ? chalk.green
+          : chalk.yellow;
+      if (pathString.length + outcomeString.length + 1 <= 80) {
+        console.info(
+          `${pathString}${new Array(80 - pathString.length - outcomeString.length - 1)
+            .fill(" ")
+            .join("")}${colorFn(outcomeString)}`
+        );
+      } else {
+        console.info(pathString);
+        console.info(
+          `${new Array(80 - outcomeString.length - 1).fill(" ").join("")}${colorFn(outcomeString)}`
+        );
+      }
+
+      if (result.err) {
+        const errString = result.err.stack
+          ? result.err.stack && indent(result.err.stack as string)
+          : `    ${JSON.stringify(result.err, null, 4)}`;
+
+        console.error(`\n${errString}\n`);
+        if (result.err.showDiff) {
+          console.error(`    Expected:\n`);
+          console.error(indent(JSON.stringify(result.err.expected, null, 4), 8));
+          console.error(`\n    Actual:\n`);
+          console.error(indent(JSON.stringify(result.err.actual, null, 4), 8));
+          console.error("\n");
+        }
+      }
+    }
+
+    const hasErrors = ctx.results.some(result => result.outcome !== "passed");
+
+    if (hasErrors) {
+      console.log(`\nTests failed.`);
+    } else {
+      console.log(`\nTests passed.`);
+    }
+
+    process.exitCode = hasErrors ? 1 : 0;
+
+    if (!Runner.noExit) {
+      process.exit();
+    }
+
+    return ctx.results;
+  }
+
+  public static async result() {
+    return await Runner.resultPromise;
+  }
+  private static noExit = false;
+
+  private static resultPromise: Promise<IRunResult[]>;
+}
