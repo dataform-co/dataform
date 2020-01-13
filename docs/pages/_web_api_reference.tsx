bd416e91667a325b2767cb8d1bfd4262248515ba
Lewis Hemens <lewis@dataform.co>
2020-01-13 14:05:26 +0000

    Add guides on how to use the Web API

---
+++ docs/pages/_web_api_reference.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { IFileTree } from "df/docs/cms";
+import { Swagger } from "df/docs/components/swagger";
+import Documentation from "df/docs/layouts/documentation";
+import { contentTree, localCms } from "df/docs/pages/_docs";
+import { NextPageContext } from "next";
+import * as React from "react";
+import { Spec } from "swagger-schema-official";
+
+interface IQuery {
+  version: string;
+}
+
+interface IProps {
+  version: string;
+  index: IFileTree;
+  current: IFileTree;
+  spec: Spec;
+  apiHost: string;
+}
+
+export default class WebApiReference extends React.Component<IProps> {
+  public static async getInitialProps(
+    context: NextPageContext & { query: IQuery }
+  ): Promise<IProps> {
+    const tree = await contentTree(localCms);
+    const apiHost = "api.dataform.co";
+
+    return {
+      version: context.query.version || "local",
+      index: tree.index(),
+      current: tree.get("dataform-web/api-reference"),
+      spec: (await axios.get(`https://${apiHost}/swagger.json`)).data,
+      apiHost
+    };
+  }
+
+  public render() {
+    return (
+      <Documentation
+        version={this.props.version}
+        current={this.props.current}
+        index={this.props.index}
+        headerLinks={Swagger.getHeaderLinks(this.props)}
+      >
+        <Swagger spec={this.props.spec} apiHost={this.props.apiHost} />
+      </Documentation>
+    );
+  }
+}
