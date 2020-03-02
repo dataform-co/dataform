a5a67e7da061547ea874aa242b8bd16f0ccf2410
Ahmad Faiyaz <faiyaz26@gmail.com>
2020-03-02 11:25:55 +0000

    rename symbol

--- api/utils/hash_object.ts
+++ api/utils/hash_object.ts
@@ -1,6 +1,6 @@
 import * as objectHash from "object-hash";
 import { dataform } from "@dataform/protos";
 
-export const hashTableDefinition = (action: dataform.IExecutionAction) => {
+export const hashExecutionAction = (action: dataform.IExecutionAction) => {
   return objectHash.sha1(action);
 };
