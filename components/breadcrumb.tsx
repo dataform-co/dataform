efee8384431ed6e30045e3e301779ffdf55c0048
Ben Birt <benjamin.birt@gmail.com>
2020-05-14 09:36:32 +0100

    ongoing

---
+++ components/breadcrumb.tsx
@@ -0,0 +1,30 @@
+import { H5, H6 } from "@blueprintjs/core";
+import * as styles from "df/components/breadcrumb.css";
+import * as React from "react";
+
+interface IBreadcrumbProps {
+  pathParts: Array<{ label: string; onClick: () => void }>;
+}
+
+export function BreadCrumb({ pathParts }: IBreadcrumbProps) {
+  return (
+    <>
+      <div className={styles.container}>
+        {pathParts.map((pathPart, index) => (
+          <>
+            <H6
+              onClick={() => {
+                if (index < pathParts.length - 1) {
+                  pathPart.onClick();
+                }
+              }}
+            >
+              {pathPart.label}
+            </H6>
+            {index < pathParts.length - 1 && " > "}
+          </>
+        ))}
+      </div>
+    </>
+  );
+}
