9937fb00c34a1c40ae59c32a542428867fdd5e39
George McGowan <george@dataform.co>
2020-05-18 16:03:10 +0100

    Add message passing between extension and webview

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -19,9 +19,9 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage("Hello World from dataform!");
   });
 
-  let currentPanel: vscode.WebviewPanel | undefined;
+  let currentPanel: vscode.WebviewPanel | null = null;
 
-  const showsidebar = vscode.commands.registerCommand("dataform.showsidebar", () => {
+  const showSidebar = vscode.commands.registerCommand("dataform.showSidebar", () => {
     // Track current webview panel
     const columnToShowIn = vscode.window.activeTextEditor
       ? vscode.window.activeTextEditor.viewColumn
@@ -31,14 +31,16 @@ export function activate(context: vscode.ExtensionContext) {
       // If we already have a panel, show it in the target column
       currentPanel.reveal(columnToShowIn);
     } else {
-      const panel = vscode.window.createWebviewPanel(
+      currentPanel = vscode.window.createWebviewPanel(
         "dataform", // Identifies the type of the webview. Used internally
         "Sidebar", // Title of the panel displayed to the user
-        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-        {} // Webview options. More on these later.
+        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.,
+        {
+          enableScripts: true
+        }
       );
 
-      panel.webview.html = getWebviewContent();
+      currentPanel.webview.html = getWebviewContent();
 
       currentPanel.onDidDispose(
         () => {
@@ -50,7 +52,13 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(showsidebar, disposable);
+  const addThingToPanel = vscode.commands.registerCommand("dataform.addToPanel", () => {
+    // Send a message to our webview.
+    // You can send any JSON serializable data.
+    currentPanel.webview.postMessage({ bodyContent: "SOME BODY CONTENT" });
+  });
+
+  context.subscriptions.push(showSidebar, addThingToPanel, disposable);
 }
 
 // this method is called when your extension is deactivated
@@ -58,14 +66,30 @@ export function deactivate() {}
 
 function getWebviewContent() {
   return `<!DOCTYPE html>
-<html lang="en">
-<head>
+  <html lang="en">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sidebar</title>
-</head>
-<body>
-    Dataform sidebar content
-</body>
-</html>`;
+    <script>
+        ${addToSidebarScript()}
+    </script>
+  </head>
+  <body>
+      <h1>Dataform sidebar content</h1>
+      <p id="sidebar-body"></p>
+  </body>
+  </html>`;
+}
+
+function addToSidebarScript() {
+  return `
+  window.addEventListener("message", event => {
+    const sidebarBody = document.getElementById("sidebar-body");
+    const message = event.data; // The JSON data our extension sent
+
+    if (message.bodyContent) {
+      sidebarBody.textContent = message.bodyContent;
+    }
+  });`;
 }
