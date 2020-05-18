7f6f7927ba88e26dccb080368f8dca63dd7c6fe2
George McGowan <george@dataform.co>
2020-05-18 14:30:44 +0100

    Simple sidebar command

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -1,27 +1,74 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "dataform" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dataform" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand("dataform.helloWorld", () => {
+    // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dataform.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Hello World from dataform!");
+  });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dataform!');
-	});
+  const showsidebar = showSidebar(context);
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(showsidebar, disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function showSidebar(context: vscode.ExtensionContext) {
+  return vscode.commands.registerCommand("dataform.showsidebar", () => {
+    // Track current webview panel
+    let currentPanel: vscode.WebviewPanel | undefined;
+    // const columnToShowIn = vscode.window.activeTextEditor
+    //   ? vscode.window.activeTextEditor.viewColumn
+    //   : undefined;
+    // // // Create and show a new webview
+    if (currentPanel) {
+      // If we already have a panel, show it in the target column
+      currentPanel.reveal(vscode.ViewColumn.Two);
+    } else {
+      const panel = vscode.window.createWebviewPanel(
+        "dataform", // Identifies the type of the webview. Used internally
+        "Sidebar", // Title of the panel displayed to the user
+        vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+
+      panel.webview.html = getWebviewContent();
+
+      currentPanel.onDidDispose(
+        () => {
+          currentPanel = undefined;
+        },
+        null,
+        context.subscriptions
+      );
+    }
+  });
+}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar</title>
+</head>
+<body>
+    Dataform sidebar content
+</body>
+</html>`;
+}
