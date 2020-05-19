0b1b60184ccaaa3af22cc9ca5274830430303bd8
Lewis Hemens <lewis@hemens.co.uk>
2020-05-19 11:42:00 +0100

    Language server hello world

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -1,6 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { workspace } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
+} from "vscode-languageclient";
+
+let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -59,6 +68,47 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(showSidebar, addThingToPanel, disposable);
+
+  // Language server stuff.
+  // The server is implemented in node
+  let serverModule = context.asAbsolutePath("server.js");
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions
+    }
+  };
+
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc")
+    }
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+
+  console.log('Congratulations, your extension "dataform" is now active!');
 }
 
 // this method is called when your extension is deactivated
