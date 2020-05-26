9ab94ca12f52f6f01647c8dd95f38c537c35927d
Ben Birt <benjamin.birt@gmail.com>
2020-05-26 13:55:24 +0100

    cleanup

--- components/breadcrumb.tsx
+++ components/breadcrumb.tsx
@@ -8,23 +8,21 @@ interface IBreadcrumbProps {
 
 export function BreadCrumb({ pathParts }: IBreadcrumbProps) {
   return (
-    <>
-      <div className={styles.container}>
-        {pathParts.map((pathPart, index) => (
-          <React.Fragment key={index}>
-            <H6
-              onClick={() => {
-                if (index < pathParts.length - 1) {
-                  pathPart.onClick();
-                }
-              }}
-            >
-              {pathPart.label}
-            </H6>
-            {index < pathParts.length - 1 && " > "}
-          </React.Fragment>
-        ))}
-      </div>
-    </>
+    <div className={styles.container}>
+      {pathParts.map((pathPart, index) => (
+        <React.Fragment key={index}>
+          <H6
+            onClick={() => {
+              if (index < pathParts.length - 1) {
+                pathPart.onClick();
+              }
+            }}
+          >
+            {pathPart.label}
+          </H6>
+          {index < pathParts.length - 1 && " > "}
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
