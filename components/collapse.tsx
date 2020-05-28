5629bc9fde35d077c312cc166e11581f7453d85b
BenBirt <benjamin.birt@gmail.com>
2020-05-28 11:57:01 +0100

    Allow configuring styles with SimpleCollapse. (#778)
    * Allow configuring styles with SimpleCollapse.
    
    * fix lewis comments

--- components/collapse.tsx
+++ components/collapse.tsx
@@ -1,21 +1,26 @@
 import { Button, Collapse } from "@blueprintjs/core";
+import * as styles from "df/components/collapse.css";
 import { useState } from "react";
 import * as React from "react";
 
-import * as styles from "df/components/collapse.css";
-
 interface ISimpleCollapseProps {
   message: React.ReactNode;
+  className?: string;
 }
 
 export function SimpleCollapse({
   message,
+  className,
   children
 }: React.PropsWithChildren<ISimpleCollapseProps>) {
   const [show, updateShow] = useState(false);
+  const cssStyles = [styles.messageContainer];
+  if (className) {
+    cssStyles.push(className);
+  }
   return (
     <>
-      <div className={styles.messageContainer}>
+      <div className={cssStyles.join(" ")}>
         {message}
         {children && (
           <Button
