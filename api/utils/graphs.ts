7b8a3d5b170ca90458acdb1b01fd1923f7fbd1e4
George McGowan <george@dataform.co>
2020-06-03 14:42:50 +0100

    Fix some typings

--- api/utils/graphs.ts
+++ api/utils/graphs.ts
@@ -1,42 +1,21 @@
 import { JSONObjectStringifier, StringifiedMap } from "df/common/strings/stringifier";
-import { IActionProto } from "df/core/session";
 import { dataform } from "df/protos/ts";
 
-const types = ["dataset", "assertion", "operation", "declaration"] as const;
-export type Type = typeof types[number];
-
-export interface IAction {
-  name: string;
-  type: Type;
-  fileName: string;
-  dependencies?: string[];
-  target?: dataform.ITarget;
-  canonicalTarget?: dataform.ITarget;
-  actionDescriptor?: dataform.IActionDescriptor;
-  tags?: string[];
-  query?: string;
-  queries?: string[];
-}
-
-export const convertGraphToActions = (graph: dataform.ICompiledGraph) => {
-  return [].concat(
-    (graph.tables || []).map(t => ({ ...t, type: "dataset" })),
-    (graph.operations || []).map(o => ({ ...o, type: "operation" })),
-    (graph.assertions || []).map(a => ({ ...a, type: "assertion" })),
-    (graph.declarations || []).map(d => ({ ...d, type: "declaration" }))
-  ) as IAction[];
+export const combineAllActions = (graph: dataform.ICompiledGraph) => {
+  return ([] as Array<
+    dataform.ITable | dataform.IOperation | dataform.IAssertion | dataform.IDeclaration
+  >).concat(
+    graph.tables || ([] as dataform.ITable[]),
+    graph.operations || ([] as dataform.IOperation[]),
+    graph.assertions || ([] as dataform.IAssertion[]),
+    graph.declarations || ([] as dataform.IDeclaration[])
+  );
 };
 
 export function actionsByTarget(compiledGraph: dataform.ICompiledGraph) {
   return new StringifiedMap(
     JSONObjectStringifier.create<dataform.ITarget>(),
-    ([] as IActionProto[])
-      .concat(
-        compiledGraph.tables || [],
-        compiledGraph.operations || [],
-        compiledGraph.assertions || [],
-        compiledGraph.declarations || []
-      )
+    combineAllActions(compiledGraph)
       // Required for backwards compatibility with old versions of @dataform/core.
       .filter(action => !!action.target)
       .map(action => [action.target, action])
@@ -46,7 +25,8 @@ export function actionsByTarget(compiledGraph: dataform.ICompiledGraph) {
 export function actionsByCanonicalTarget(compiledGraph: dataform.ICompiledGraph) {
   return new StringifiedMap(
     JSONObjectStringifier.create<dataform.ITarget>(),
-    convertGraphToActions(compiledGraph)
+    combineAllActions(compiledGraph)
+      // Required for backwards compatibility with old versions of @dataform/core.
       .filter(action => !!action.canonicalTarget)
       .map(action => [action.canonicalTarget, action])
   );
