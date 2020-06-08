c33b46e7c9ea2e88cccd7eb73593f7b61f1e1616
Elias Kassell <elias@dataform.co>
2020-06-08 12:12:43 +0100

    Contextual ops (#799)
    * Added proto options and configuration, test should be integration rather than api spec
    
    * Working context ops for BQ and azure
    
    * Tidy, removed unused code
    
    * Fixed bigquery and azure integration tests
    
    * Remove per file contextual ops functioanlity, updated tests
    
    * Ben comments
    
    * Remove console.logs
    
    * Final ben comments
    
    * Add --build_tests_only to CI tests
    
    * Fix CLI test

--- core/adapters/sqldatawarehouse.ts
+++ core/adapters/sqldatawarehouse.ts
@@ -52,6 +52,10 @@ export class SQLDataWarehouseAdapter extends Adapter implements IAdapter {
 
     this.postOps(table, runConfig, tableMetadata).forEach(statement => tasks.add(statement));
 
+    if (runConfig.useSingleQueryPerAction) {
+      return tasks.concatenate();
+    }
+
     return tasks;
   }
 
