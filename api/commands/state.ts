dccd3148f80216582fc96165e3d3102e320e3f77
Ben Birt <benjamin.birt@gmail.com>
2020-06-09 13:15:25 +0100

    include all actions in warehouse state

--- api/commands/state.ts
+++ api/commands/state.ts
@@ -5,10 +5,15 @@ export async function state(
   compiledGraph: dataform.ICompiledGraph,
   dbadapter: IDbAdapter
 ): Promise<dataform.IWarehouseState> {
-  const allTables = await Promise.all(
-    compiledGraph.tables.map(async t => dbadapter.table(t.target))
-  );
-  // filter out tables that don't exist
+  const allTables = await Promise.all([
+    ...compiledGraph.tables.map(async table => dbadapter.table(table.target)),
+    ...compiledGraph.operations
+      .filter(operation => operation.hasOutput)
+      .map(async operation => dbadapter.table(operation.target)),
+    ...compiledGraph.assertions.map(async assertion => dbadapter.table(assertion.target))
+  ]);
+
+  // Filter out datasets that don't exist.
   const tablesWithValues = allTables.filter(table => {
     return !!table && !!table.type;
   });
@@ -19,9 +24,9 @@ export async function state(
     try {
       cachedStates = await dbadapter.persistedStateMetadata();
     } catch (err) {
-      // if the table doesn't exist or for some network error
+      // If the table doesn't exist or for some network error
       // cache state is not fetchable, then return empty array
-      // which implies no caching will be done
+      // which implies no caching will be done.
       cachedStates = [];
     }
   }
