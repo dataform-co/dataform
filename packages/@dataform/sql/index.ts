d2c44c2e03c48a52da8c95f2009233df63282f02
Lewis Hemens <lewis@hemens.co.uk>
2020-06-25 17:17:48 +0100

    Don't blow up outside dataform

--- packages/@dataform/sql/index.ts
+++ packages/@dataform/sql/index.ts
@@ -1,10 +1,19 @@
 import { ISqlDialect, Sql } from "df/sql";
 
-const currentDataformDialect = ({
-  bigquery: "standard",
-  redshift: "postgres",
-  snowflake: "snowflake",
-  sqldatawarehouse: "mssql"
-} as { [key: string]: ISqlDialect })[(global as any).session.config.warehouse];
+/**
+ * If we are in a dataform context, try to infer the current sql dialect, otherwise use "standard" as a default.
+ */
+const getDefaultDialect = () => {
+  const dataformWarehouse = (global as any).session?.config?.warehouse;
+  if (!!dataformWarehouse) {
+    return "standard";
+  }
+  return ({
+    bigquery: "standard",
+    redshift: "postgres",
+    snowflake: "snowflake",
+    sqldatawarehouse: "mssql"
+  } as { [key: string]: ISqlDialect })[dataformWarehouse];
+};
 
-module.exports = (dialect: ISqlDialect = currentDataformDialect) => new Sql(dialect);
+module.exports = (dialect: ISqlDialect = getDefaultDialect()) => new Sql(dialect);
