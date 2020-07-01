d849ec89f7ae1e4d2c2bf0f1c058c0e10c72031a
Elias Kassell <elias@dataform.co>
2020-07-01 10:34:14 +0100

    Changed back to number

--- core/column_descriptors.ts
+++ core/column_descriptors.ts
@@ -101,8 +101,8 @@ export class ColumnDescriptors {
         return dataform.ColumnDescriptor.DimensionType.CATEGORY;
       case "timestamp":
         return dataform.ColumnDescriptor.DimensionType.TIMESTAMP;
-      case "numerical":
-        return dataform.ColumnDescriptor.DimensionType.NUMERICAL;
+      case "number":
+        return dataform.ColumnDescriptor.DimensionType.NUMBER;
       case undefined:
         return undefined;
       default:
@@ -116,8 +116,8 @@ export class ColumnDescriptors {
         return "category";
       case dataform.ColumnDescriptor.DimensionType.TIMESTAMP:
         return "timestamp";
-      case dataform.ColumnDescriptor.DimensionType.NUMERICAL:
-        return "numerical";
+      case dataform.ColumnDescriptor.DimensionType.NUMBER:
+        return "number";
       case dataform.ColumnDescriptor.DimensionType.UNKNOWN_DIMENSION:
         return undefined;
       case undefined:
