26a3d1ab064e6baf77055c62c853c5d28ef68d5b
Elias Kassell <elias@dataform.co>
2020-07-16 15:47:56 +0100

    Tidied props

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,25 +1,29 @@
-import {
-  Intent,
-  ISpinnerProps as IBlueprintSpinnerProps,
-  Spinner as BlueprintSpinner
-} from "@blueprintjs/core";
-import * as styles from "df/components/spinner.css";
+import { IIntentProps, Intent, Spinner as BlueprintSpinner } from "@blueprintjs/core";
 import * as React from "react";
 
-interface ISpinnerProps extends IBlueprintSpinnerProps {
-  small?: boolean;
-  large?: boolean;
+import * as styles from "df/components/spinner.css";
+
+export type SpinnerSize = "large" | "standard" | "small";
+
+export interface ISpinnerProps extends IIntentProps {
+  size?: SpinnerSize;
+  value?: number;
 }
 
-export function Spinner({ small, large, ...rest }: ISpinnerProps) {
-  const size = !!large
-    ? BlueprintSpinner.SIZE_LARGE
-    : !!small
-    ? BlueprintSpinner.SIZE_SMALL
-    : BlueprintSpinner.SIZE_STANDARD;
+export function Spinner({ size, ...rest }: ISpinnerProps) {
   return (
     <div className={styles.centered}>
-      <BlueprintSpinner intent={Intent.PRIMARY} size={size} {...rest} />
+      <BlueprintSpinner
+        intent={Intent.PRIMARY}
+        size={
+          size === "large"
+            ? BlueprintSpinner.SIZE_LARGE
+            : size === "small"
+            ? BlueprintSpinner.SIZE_SMALL
+            : BlueprintSpinner.SIZE_STANDARD
+        }
+        {...rest}
+      />
     </div>
   );
 }
