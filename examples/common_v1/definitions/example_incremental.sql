20419a105bbb3dd8bb7d098bd72e1616dd78c67d
Lewis Hemens <lewis@hemens.co.uk>
2020-07-29 08:54:40 +0100

    Fix tests

--- examples/common_v1/definitions/example_incremental.sql
+++ examples/common_v1/definitions/example_incremental.sql
@@ -1,4 +1,4 @@
 --js type("incremental");
 --js where(ctx => `ts > (select max(ts) from ${self()}) or (select max(ts) from ${self()}) is null`)
 --js const sql = require("@dataform/sql");
-select ${sql().timestampCurrentUTC()} as ts
+select ${sql().timestamps.currentUTC()} as ts
