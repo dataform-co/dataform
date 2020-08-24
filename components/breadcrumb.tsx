65a4687795fcc2f962d56283c83b7d5734df59b5
Elias Kassell <elias@dataform.co>
2020-08-24 13:39:44 +0100

    Add react router link to breadcrumb option

--- components/breadcrumb.tsx
+++ components/breadcrumb.tsx
@@ -1,10 +1,11 @@
 import { H6 } from "@blueprintjs/core";
 import * as React from "react";
+import { Link } from "react-router";
 
 import * as styles from "df/components/breadcrumb.css";
 
 export interface IBreadCrumbProps {
-  pathParts: Array<{ label: string; onClick: () => void }>;
+  pathParts: Array<{ label: string; onClick?: () => void; to?: string }>;
 }
 
 export function BreadCrumb({ pathParts }: IBreadCrumbProps) {
@@ -13,7 +14,13 @@ export function BreadCrumb({ pathParts }: IBreadCrumbProps) {
       {pathParts.slice(0, -1).map((pathPart, index) => (
         <React.Fragment key={index}>
           <H6>
-            <a onClick={pathPart.onClick}>{pathPart.label}</a>
+            {!!pathPart.to ? (
+              <Link onClick={pathPart.onClick} to={pathPart.to}>
+                {pathPart.label}
+              </Link>
+            ) : (
+              <a onClick={pathPart.onClick}>{pathPart.label}</a>
+            )}
           </H6>
           {"  >  "}
         </React.Fragment>
