3cf0af19c827f1ddafd39e8e94e51655a304b30f
Elias Kassell <elias@dataform.co>
2020-08-26 16:42:18 +0100

    Add encoded credentials, testing file and project

---
+++ tests/integration/postgres.spec.ts
@@ -0,0 +1,230 @@
+import { expect } from "chai";
+
+import * as dfapi from "df/api";
+import * as dbadapters from "df/api/dbadapters";
+import * as adapters from "df/core/adapters";
+import { dataform } from "df/protos/ts";
+import { suite, test } from "df/testing";
+import { compile, getTableRows, keyBy } from "df/tests/integration/utils";
+
+suite("@dataform/integration/postgres", { parallel: true }, ({ before, after }) => {
+  const credentials = dfapi.credentials.read("postgres", "test_credentials/postgres.json");
+
+  let dbadapter: dbadapters.IDbAdapter;
+
+  before("create adapter", async () => {
+    dbadapter = await dbadapters.create(credentials, "postgres");
+  });
+
+  after("close adapter", async () => dbadapter.close());
+
+  test("run", { timeout: 60000 }, async () => {
+    const compiledGraph = await compile("tests/integration/postgres_project", "project_e2e");
+
+    const adapter = adapters.create(compiledGraph.projectConfig, compiledGraph.dataformCoreVersion);
+
+    // Run the project.
+    let executionGraph = await dfapi.build(compiledGraph, {}, dbadapter);
+    let executedGraph = await dfapi.run(dbadapter, executionGraph).result();
+
+    const actionMap = keyBy(executedGraph.actions, v => v.name);
+    expect(Object.keys(actionMap).length).eql(13);
+
+    // Check the status of action execution.
+    const expectedFailedActions = [
+      "df_integration_test_assertions_project_e2e.example_assertion_uniqueness_fail",
+      "df_integration_test_assertions_project_e2e.example_assertion_fail"
+    ];
+    for (const actionName of Object.keys(actionMap)) {
+      const expectedResult = expectedFailedActions.includes(actionName)
+        ? dataform.ActionResult.ExecutionStatus.FAILED
+        : dataform.ActionResult.ExecutionStatus.SUCCESSFUL;
+      expect(actionMap[actionName].status).equals(
+        expectedResult,
+        actionMap[actionName].tasks.map(task => task.errorMessage).join("\n")
+      );
+    }
+
+    expect(
+      actionMap["df_integration_test_assertions_project_e2e.example_assertion_uniqueness_fail"]
+        .tasks[2].errorMessage
+      // TODO: Update this error check to be correct.
+    ).to.eql("redshift error: Assertion failed: query returned 1 row(s).");
+
+    // Check the data in the incremental table.
+    let incrementalTable = keyBy(compiledGraph.tables, t => t.name)[
+      "df_integration_test_project_e2e.example_incremental"
+    ];
+    let incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
+    expect(incrementalRows.length).equals(3);
+
+    // Check the data in the incremental merge table.
+    incrementalTable = keyBy(compiledGraph.tables, t => t.name)[
+      "df_integration_test_project_e2e.example_incremental_merge"
+    ];
+    incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
+    expect(incrementalRows.length).equals(2);
+
+    // Re-run some of the actions.
+    executionGraph = await dfapi.build(
+      compiledGraph,
+      {
+        actions: [
+          "example_incremental",
+          "example_incremental_merge",
+          "example_table",
+          "example_view"
+        ]
+      },
+      dbadapter
+    );
+    executedGraph = await dfapi.run(dbadapter, executionGraph).result();
+    expect(executedGraph.status).equals(
+      dataform.RunResult.ExecutionStatus.SUCCESSFUL,
+      executedGraph.actions
+        .map(action => action.tasks.map(task => task.errorMessage).join("\n"))
+        .join("\n")
+    );
+
+    // Check there are the expected number of extra rows in the incremental table.
+    incrementalTable = keyBy(compiledGraph.tables, t => t.name)[
+      "df_integration_test_project_e2e.example_incremental"
+    ];
+    incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
+    expect(incrementalRows.length).equals(5);
+
+    // Check there are the expected number of extra rows in the incremental merge table.
+    incrementalTable = keyBy(compiledGraph.tables, t => t.name)[
+      "df_integration_test_project_e2e.example_incremental_merge"
+    ];
+    incrementalRows = await getTableRows(incrementalTable.target, adapter, dbadapter);
+    expect(incrementalRows.length).equals(2);
+  });
+
+  // test("run unit tests", async () => {
+  //   const compiledGraph = await compile("tests/integration/postgres_project", "unit_tests");
+
+  //   // Run the tests.
+  //   const testResults = await dfapi.test(dbadapter, compiledGraph.tests);
+  //   expect(testResults).to.eql([
+  //     { name: "successful", successful: true },
+  //     {
+  //       name: "expected more rows than got",
+  //       successful: false,
+  //       messages: ["Expected 3 rows, but saw 2 rows."]
+  //     },
+  //     {
+  //       name: "expected fewer columns than got",
+  //       successful: false,
+  //       messages: ['Expected columns "col1,col2,col3", but saw "col1,col2,col3,col4".']
+  //     },
+  //     {
+  //       name: "wrong columns",
+  //       successful: false,
+  //       messages: ['Expected columns "col1,col2,col3,col4", but saw "col1,col2,col3,col5".']
+  //     },
+  //     {
+  //       name: "wrong row contents",
+  //       successful: false,
+  //       messages: [
+  //         'For row 0 and column "col2": expected "1", but saw "5".',
+  //         'For row 1 and column "col3": expected "6.5", but saw "12.0".',
+  //         'For row 2 and column "col1": expected "sup?", but saw "WRONG".'
+  //       ]
+  //     }
+  //   ]);
+  // });
+
+  // suite("query limits work", { parallel: true }, async () => {
+  //   const query = `
+  //     select 1 union all
+  //     select 2 union all
+  //     select 3 union all
+  //     select 4 union all
+  //     select 5`;
+
+  //   for (const options of [
+  //     { interactive: true, rowLimit: 2 },
+  //     { interactive: false, rowLimit: 2 },
+  //     { interactive: true, byteLimit: 50 },
+  //     { interactive: false, byteLimit: 50 }
+  //   ]) {
+  //     test(`with options=${JSON.stringify(options)}`, async () => {
+  //       const { rows } = await dbadapter.execute(query, options);
+  //       expect(rows).to.eql([
+  //         {
+  //           "?column?": 1
+  //         },
+  //         {
+  //           "?column?": 2
+  //         }
+  //       ]);
+  //     });
+  //   }
+  // });
+
+  // suite("evaluate", () => {
+  //   test("evaluate from valid compiled graph as valid", async () => {
+  //     const compiledGraph = await compile("tests/integration/postgres_project", "evaluate");
+  //     const executionGraph = await dfapi.build(compiledGraph, {}, dbadapter);
+  //     await dfapi.run(dbadapter, executionGraph).result();
+
+  //     const view = keyBy(compiledGraph.tables, t => t.name)[
+  //       "df_integration_test_evaluate.example_view"
+  //     ];
+  //     let evaluations = await dbadapter.evaluate(dataform.Table.create(view));
+  //     expect(evaluations.length).to.equal(1);
+  //     expect(evaluations[0].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.SUCCESS
+  //     );
+
+  //     const table = keyBy(compiledGraph.tables, t => t.name)[
+  //       "df_integration_test_evaluate.example_table"
+  //     ];
+  //     evaluations = await dbadapter.evaluate(dataform.Table.create(table));
+  //     expect(evaluations.length).to.equal(1);
+  //     expect(evaluations[0].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.SUCCESS
+  //     );
+
+  //     const assertion = keyBy(compiledGraph.assertions, t => t.name)[
+  //       "df_integration_test_assertions_evaluate.example_assertion_pass"
+  //     ];
+  //     evaluations = await dbadapter.evaluate(dataform.Assertion.create(assertion));
+  //     expect(evaluations.length).to.equal(1);
+  //     expect(evaluations[0].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.SUCCESS
+  //     );
+
+  //     const incremental = keyBy(compiledGraph.tables, t => t.name)[
+  //       "df_integration_test_evaluate.example_incremental"
+  //     ];
+  //     evaluations = await dbadapter.evaluate(dataform.Table.create(incremental));
+  //     expect(evaluations.length).to.equal(2);
+  //     expect(evaluations[0].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.SUCCESS
+  //     );
+  //     expect(evaluations[1].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.SUCCESS
+  //     );
+  //   });
+
+  //   test("invalid table fails validation", async () => {
+  //     const evaluations = await dbadapter.evaluate(
+  //       dataform.Table.create({
+  //         type: "table",
+  //         query: "thisisillegal",
+  //         target: {
+  //           schema: "df_integration_test",
+  //           name: "example_illegal_table",
+  //           database: "dataform-integration-tests"
+  //         }
+  //       })
+  //     );
+  //     expect(evaluations.length).to.equal(1);
+  //     expect(evaluations[0].status).to.equal(
+  //       dataform.QueryEvaluation.QueryEvaluationStatus.FAILURE
+  //     );
+  //   });
+  // });
+});
