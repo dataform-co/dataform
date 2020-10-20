3f2196689d9d1fbd67ef1930a05673bce81a18db
Elias Kassell <elias@dataform.co>
2020-10-20 13:27:36 +0100

    Hey Presto (#987)
    * Add working presto bazel build
    
    * Add presto communication using custom http requests
    
    * Swapped to presto node client, added types, tidied up fixture and re added docker run testing
    
    * Remove yarn error log
    
    * Add basic core adapter, begin on API adapter
    
    * Added API (with many missing features), ready for some basic integration tests
    
    * Fixed integration tests, working! However, sets catalog and schema in connection due to library being incorrect
    
    * Fix query evaluation and schema and table listing
    
    * Added promise pool, a better way of limiting rows, added error types, moved presto execute to only within the adapter, made table in adapter more similar to other adapters, tidied tables, added target failure test
    
    * Fixed thrown error for no table metadata found, tidied various bits of tests
    
    * Removed unused code, ben comments
    
    * Minor tidying, fix build error
    
    * Add presto-client to externals list
    
    * Update bazel test script to use cloud build network flag, and postgres
    
    * Swapped back to using environment variable for network flag due to protomongo whinging
    
    * Fix address resolution issue for presto test
    
    * Add console log to error thrown by presto connector
    
    * Fix message
    
    * modify
    
    * Try a different port
    
    * Made docker output show
    
    * Simplify credential usage
    
    * remove weird failure line
    
    * presto reconfigured
    
    * Progress debugging bazel build for presto image
    
    * Fix file volume mounting, add presto catalog connections
    
    * Try removing jvm volume mounting
    
    * Simplify file path joining, add debug log
    
    * Add dot to path, add ls exec contents
    
    * Add docker layers for loading in image
    
    * Remove rogue log
    
    Co-authored-by: Ben Birt <benjamin.birt@gmail.com>

--- api/dbadapters/index.ts
+++ api/dbadapters/index.ts
@@ -1,6 +1,7 @@
 import { Credentials } from "df/api/commands/credentials";
 import { BigQueryDbAdapter } from "df/api/dbadapters/bigquery";
 import { PostgresDbAdapter } from "df/api/dbadapters/postgres";
+import { PrestoDbAdapter } from "df/api/dbadapters/presto";
 import { RedshiftDbAdapter } from "df/api/dbadapters/redshift";
 import { SnowflakeDbAdapter } from "df/api/dbadapters/snowflake";
 import { SQLDataWarehouseDBAdapter } from "df/api/dbadapters/sqldatawarehouse";
@@ -46,6 +47,7 @@ export interface IDbAdapter extends IDbClient {
   schemas(database: string): Promise<string[]>;
   createSchema(database: string, schema: string): Promise<void>;
 
+  // TODO: This should take parameters to allow for retrieving from a specific database/schema.
   tables(): Promise<dataform.ITarget[]>;
   search(searchText: string, options?: { limit: number }): Promise<dataform.ITableMetadata[]>;
   table(target: dataform.ITarget): Promise<dataform.ITableMetadata>;
@@ -70,8 +72,13 @@ export interface IDbAdapter extends IDbClient {
   close(): Promise<void>;
 }
 
+interface ICredentialsOptions {
+  concurrencyLimit?: number;
+  disableSslForTestsOnly?: boolean;
+}
+
 export interface IDbAdapterClass<T extends IDbAdapter> {
-  create: (credentials: Credentials, options?: { concurrencyLimit?: number }) => Promise<T>;
+  create: (credentials: Credentials, options: ICredentialsOptions) => Promise<T>;
 }
 
 const registry: { [warehouseType: string]: IDbAdapterClass<IDbAdapter> } = {};
@@ -80,10 +87,19 @@ export function register(warehouseType: string, c: IDbAdapterClass<IDbAdapter>) 
   registry[warehouseType] = c;
 }
 
+export const validWarehouses = [
+  "bigquery",
+  "postgres",
+  "redshift",
+  "sqldatawarehouse",
+  "snowflake",
+  "presto"
+];
+
 export async function create(
   credentials: Credentials,
-  warehouseType: string,
-  options?: { concurrencyLimit?: number; disableSslForTestsOnly?: boolean }
+  warehouseType: typeof validWarehouses[number],
+  options?: ICredentialsOptions
 ): Promise<IDbAdapter> {
   if (!registry[warehouseType]) {
     throw new Error(`Unsupported warehouse: ${warehouseType}`);
@@ -96,3 +112,4 @@ register("postgres", PostgresDbAdapter);
 register("redshift", RedshiftDbAdapter);
 register("snowflake", SnowflakeDbAdapter);
 register("sqldatawarehouse", SQLDataWarehouseDBAdapter);
+register("presto", PrestoDbAdapter);
