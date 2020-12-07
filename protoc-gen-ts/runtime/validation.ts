3da416672d8e262e8e9cec9ded7a630333143570
Ben Birt <benjamin.birt@gmail.com>
2020-12-07 10:46:48 +0000

    elias comments

--- protoc-gen-ts/runtime/validation.ts
+++ protoc-gen-ts/runtime/validation.ts
@@ -1,40 +1,50 @@
 import Long from "long";
 
-const TWO_TO_31 = Math.pow(2, 31);
-const NEGATIVE_TWO_TO_31 = -TWO_TO_31;
-const TWO_TO_32 = Math.pow(2, 32);
+const MIN_INCLUSIVE_SIGNED_32_BIT_INT = -Math.pow(2, 31);
+const MAX_EXCLUSIVE_SIGNED_32_BIT_INT = -MIN_INCLUSIVE_SIGNED_32_BIT_INT;
 
-const MIN_32_BIT_FLOAT = -3.4028234663852886e38;
-const MAX_32_BIT_FLOAT = 3.4028234663852886e38;
+const MIN_INCLUSIVE_UNSIGNED_32_BIT_INT = 0;
+const MAX_EXCLUSIVE_UNSIGNED_32_BIT_INT = Math.pow(2, 32);
 
-const MIN_64_BIT_FLOAT = -1.7976931348623157e308;
-const MAX_64_BIT_FLOAT = 1.7976931348623157e308;
+const MIN_INCLUSIVE_32_BIT_FLOAT = -3.4028234663852886e38;
+const MAX_INCLUSIVE_32_BIT_FLOAT = 3.4028234663852886e38;
+
+const MIN_INCLUSIVE_64_BIT_FLOAT = -1.7976931348623157e308;
+const MAX_INCLUSIVE_64_BIT_FLOAT = 1.7976931348623157e308;
 
 export function checkSignedInt32(val: number | number[]) {
   if (Array.isArray(val)) {
-    val.forEach(singleVal => check32BitInteger(NEGATIVE_TWO_TO_31, TWO_TO_31, singleVal));
+    val.forEach(singleVal =>
+      check32BitInteger(MIN_INCLUSIVE_SIGNED_32_BIT_INT, MAX_EXCLUSIVE_SIGNED_32_BIT_INT, singleVal)
+    );
     return;
   }
-  check32BitInteger(NEGATIVE_TWO_TO_31, TWO_TO_31, val);
+  check32BitInteger(MIN_INCLUSIVE_SIGNED_32_BIT_INT, MAX_EXCLUSIVE_SIGNED_32_BIT_INT, val);
 }
 
 export function checkUnsignedInt32(val: number | number[]) {
   if (Array.isArray(val)) {
-    val.forEach(singleVal => check32BitInteger(0, TWO_TO_32, singleVal));
+    val.forEach(singleVal =>
+      check32BitInteger(
+        MIN_INCLUSIVE_UNSIGNED_32_BIT_INT,
+        MAX_EXCLUSIVE_UNSIGNED_32_BIT_INT,
+        singleVal
+      )
+    );
     return;
   }
-  check32BitInteger(0, TWO_TO_32, val);
+  check32BitInteger(MIN_INCLUSIVE_UNSIGNED_32_BIT_INT, MAX_EXCLUSIVE_UNSIGNED_32_BIT_INT, val);
 }
 
-function check32BitInteger(min: number, max: number, val: number) {
+function check32BitInteger(minInclusive: number, maxExclusive: number, val: number) {
   if (val - Math.floor(val) !== 0) {
     throw new Error(`${val} is not an integer.`);
   }
-  if (val < min) {
-    throw new Error(`${val} is less than ${min}.`);
+  if (val < minInclusive) {
+    throw new Error(`${val} is less than ${minInclusive}.`);
   }
-  if (val >= max) {
-    throw new Error(`${val} is greater than or equal to ${max}.`);
+  if (val >= maxExclusive) {
+    throw new Error(`${val} is greater than or equal to ${maxExclusive}.`);
   }
 }
 
@@ -62,28 +72,32 @@ function check64BitInteger(unsigned: boolean, val: Long) {
 
 export function check32BitFloat(val: number | number[]) {
   if (Array.isArray(val)) {
-    val.forEach(singleVal => checkFloat(MIN_32_BIT_FLOAT, MAX_32_BIT_FLOAT, singleVal));
+    val.forEach(singleVal =>
+      checkFloat(MIN_INCLUSIVE_32_BIT_FLOAT, MAX_INCLUSIVE_32_BIT_FLOAT, singleVal)
+    );
     return;
   }
-  checkFloat(MIN_32_BIT_FLOAT, MAX_32_BIT_FLOAT, val);
+  checkFloat(MIN_INCLUSIVE_32_BIT_FLOAT, MAX_INCLUSIVE_32_BIT_FLOAT, val);
 }
 
 export function check64BitFloat(val: number | number[]) {
   if (Array.isArray(val)) {
-    val.forEach(singleVal => checkFloat(MIN_64_BIT_FLOAT, MAX_64_BIT_FLOAT, singleVal));
+    val.forEach(singleVal =>
+      checkFloat(MIN_INCLUSIVE_64_BIT_FLOAT, MAX_INCLUSIVE_64_BIT_FLOAT, singleVal)
+    );
     return;
   }
-  checkFloat(MIN_64_BIT_FLOAT, MAX_64_BIT_FLOAT, val);
+  checkFloat(MIN_INCLUSIVE_64_BIT_FLOAT, MAX_INCLUSIVE_64_BIT_FLOAT, val);
 }
 
-function checkFloat(min: number, max: number, val: number) {
+function checkFloat(minInclusive: number, maxInclusive: number, val: number) {
   if (val === Number.POSITIVE_INFINITY || val === Number.NEGATIVE_INFINITY || val === Number.NaN) {
     return;
   }
-  if (val < min) {
-    throw new Error(`${val} is less than ${min}.`);
+  if (val < minInclusive) {
+    throw new Error(`${val} is less than ${minInclusive}.`);
   }
-  if (val > max) {
-    throw new Error(`${val} is greater than ${max}.`);
+  if (val > maxInclusive) {
+    throw new Error(`${val} is greater than ${maxInclusive}.`);
   }
 }
