2c121361126cfe2d7dcef94882e52cf9e82e50a8
Elias Kassell <elias@dataform.co>
2021-01-11 17:42:30 +0000

    Progress using non fork method, but should probably ue forking method

--- sandbox/executor.cc
+++ sandbox/executor.cc
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// A demo sandbox for the custom_fork_bin binary.
-// Use: custom_fork_sandbox --logtostderr
-
 #include <syscall.h>
 
 #include <cstdlib>
@@ -40,93 +37,91 @@ std::unique_ptr<sandbox2::Policy> GetPolicy() {
   return sandbox2::PolicyBuilder()
       // The most frequent syscall should go first in this sequence (to make it
       // fast).
+      .AllowRead()
+      .AllowWrite()
       .AllowExit()
       .AllowTime()
       .EnableNamespaces()
+      .AllowSyscalls({
+        __NR_close, __NR_getpid,
+#if defined(__NR_arch_prctl)
+            // Not defined with every CPU architecture in Prod.
+            __NR_arch_prctl,
+#endif  // defined(__NR_arch_prctl)
+      })
+#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
+    defined(THREAD_SANITIZER)
+      .AllowMmap()
+#endif
       .BuildOrDie();
 }
 
-static int SandboxIteration(sandbox2::ForkClient* fork_client, int32_t i) {
-  // Now, start the sandboxee as usual, just use a different Executor
-  // constructor, which takes pointer to the ForkClient.
-  auto executor = absl::make_unique<sandbox2::Executor>(fork_client);
+bool RunNodeScript(sandbox2::Comms* comms, std::string script, std::string* response) {
+  const uint8_t* buf = reinterpret_cast<const uint8_t*>(script.data());
+  size_t buf_size = script.size();
 
-  // Set limits as usual.
-  executor
-      ->limits()
-      // Remove restrictions on the size of address-space of sandboxed
-      // processes. Here, it's 1GiB.
-      ->set_rlimit_as(
-#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-    defined(THREAD_SANITIZER)
-          RLIM64_INFINITY
-#else
-          1ULL << 30  // 1GiB
-#endif
-          )
-      // Kill sandboxed processes with a signal (SIGXFSZ) if it writes more than
-      // these many bytes to the file-system (including logs in prod, which
-      // write to files STDOUT and STDERR).
-      .set_rlimit_fsize(1024 /* bytes */)
-      // The CPU time limit.
-      .set_rlimit_cpu(10 /* CPU-seconds */)
-      .set_walltime_limit(absl::Seconds(5));
-
-  auto* comms = executor->ipc()->comms();
-
-  auto policy = GetPolicy();
-  sandbox2::Sandbox2 s2(std::move(executor), std::move(policy));
-
-  // Let the sandboxee run (asynchronously).
-  CHECK(s2.RunAsync());
-  // Send integer, which will be returned as the sandboxee's exit code.
-  CHECK(comms->SendInt32(i));
-  auto result = s2.AwaitResult();
-
-  LOG(INFO) << "Final execution status of PID " << s2.GetPid() << ": "
-            << result.ToString();
-
-  if (result.final_status() != sandbox2::Result::OK) {
-    return -1;
+  if (!comms->SendBytes(buf, buf_size)) {
+    LOG(ERROR) << "RunNodeScript_comms->SendBytes() failed";
+    return false;
   }
-  return result.reason_code();
+
+  if (!comms->RecvString(response)) {
+    LOG(ERROR) << "RunNodeScript_comms->RecvString failed";
+    return false;
+  }
+  return true;
 }
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
 
-  // This test is incompatible with sanitizers.
-  // The `SKIP_SANITIZERS_AND_COVERAGE` macro won't work for us here since we
-  // need to return something.
-#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-    defined(THREAD_SANITIZER)
-  return EXIT_SUCCESS;
-#endif
-
-  // Start a custom fork-server (via sandbox2::Executor).
   const std::string path = "/usr/bin/node";
-  std::vector<std::string> args = {path};
+  std::vector<std::string> args = {path, "/usr/local/google/home/eliaskassell/Documents/github/dataform/tmp.js"};
   std::vector<std::string> envs = {};
-  auto fork_executor = absl::make_unique<sandbox2::Executor>(path, args, envs);
-  // Start the fork-server (which is here: the custom_fork_bin process calling
-  // sandbox2::Client::WaitAndFork() in a loop).
-  //
-  // This function returns immediately, returning std::unique_ptr<ForkClient>.
-  //
-  // If it's holding the nullptr, then this call had failed.
-  auto fork_client = fork_executor->StartForkServer();
-  if (!fork_client) {
-    LOG(ERROR) << "Starting custom ForkServer failed";
-    return EXIT_FAILURE;
-  }
-  LOG(INFO) << "Custom Fork-Server started";
+  auto executor = absl::make_unique<sandbox2::Executor>(path, args, envs);
+  
+  executor->set_enable_sandbox_before_exec(true);
 
-  // Test new sandboxees: send them integers over Comms, and expect they will
-  // exit with these specific exit codes.
-  for (int i = 0; i < 10; i++) {
-    CHECK_EQ(SandboxIteration(fork_client.get(), i), i);
-  }
+  auto* comms = executor->ipc()->comms();
+  auto policy = GetPolicy();
 
+  sandbox2::Sandbox2 s2(std::move(executor), std::move(policy));
+
+  // Let the sandboxee run.
+  if (!s2.RunAsync()) {
+    auto result = s2.AwaitResult();
+    LOG(ERROR) << "RunAsync failed: " << result.ToString();
+    return 2;
+  }
+  
+  std::string script = "console.log('HeLlO tHeRe')";
+  std::string response;
+
+  // if (!RunNodeScript(comms, script, &response)) {
+  //   LOG(ERROR) << "Sending failed";
+  //   if (!s2.IsTerminated()) {
+  //     // Kill the sandboxee, because failure to receive the data over the Comms
+  //     // channel doesn't automatically mean that the sandboxee itself had
+  //     // already finished. The final reason will not be overwritten, so if
+  //     // sandboxee finished because of e.g. timeout, the TIMEOUT reason will be
+  //     // reported.
+  //     LOG(INFO) << "Killing sandboxee";
+  //     s2.Kill();
+  //   }
+  // }
+
+  auto result = s2.AwaitResult();
+  if (result.final_status() != sandbox2::Result::OK) {
+    LOG(ERROR) << "Sandbox error: " << result.ToString();
+    return 3;  // e.g. sandbox violation, signal (sigsegv)
+  }
+  auto code = result.reason_code();
+  if (code) {
+    LOG(ERROR) << "Sandboxee exited with non-zero: " << code;
+    return 4;  // e.g. normal child error
+  }
+  LOG(INFO) << "Sandboxee finished: " << result.ToString();
+  printf("RESPONSE: %s\n", response.c_str());
   return EXIT_SUCCESS;
 }
