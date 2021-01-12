c20991387e3ab1f47b19805d9b93c97b5e88d08e
Elias Kassell <elias@dataform.co>
2021-01-12 12:10:18 +0000

    Fixed policy enforcement of node process, but still execveat error. Should try using bazel pathed nodejs version

--- sandbox/executor.cc
+++ sandbox/executor.cc
@@ -12,8 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <syscall.h>
+// Based on this example:
+// https://github.com/google/sandboxed-api/blob/master/sandboxed_api/sandbox2/examples/static/static_sandbox.cc
 
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <syscall.h>
+#include <unistd.h>
+
+#include <csignal>
 #include <cstdlib>
 #include <memory>
 #include <string>
@@ -23,105 +30,119 @@
 #include <glog/logging.h>
 #include "sandboxed_api/util/flag.h"
 #include "absl/memory/memory.h"
-#include "sandboxed_api/sandbox2/comms.h"
 #include "sandboxed_api/sandbox2/executor.h"
-#include "sandboxed_api/sandbox2/forkserver.h"
 #include "sandboxed_api/sandbox2/limits.h"
 #include "sandboxed_api/sandbox2/policy.h"
 #include "sandboxed_api/sandbox2/policybuilder.h"
 #include "sandboxed_api/sandbox2/result.h"
 #include "sandboxed_api/sandbox2/sandbox2.h"
+#include "sandboxed_api/sandbox2/util/bpf_helper.h"
 #include "sandboxed_api/sandbox2/util/runfiles.h"
+
+const std::string NODE_PATH = "/usr/bin/node";
 
 std::unique_ptr<sandbox2::Policy> GetPolicy() {
   return sandbox2::PolicyBuilder()
       // The most frequent syscall should go first in this sequence (to make it
       // fast).
+      // Allow read() with all arguments.
       .AllowRead()
       .AllowWrite()
-      .AllowExit()
-      .AllowTime()
+      // Allow a preset of syscalls that are known to be used during startup
+      // of static binaries.
+      .AllowStaticStartup()
       .EnableNamespaces()
-      .AllowSyscalls({
-        __NR_close, __NR_getpid,
-#if defined(__NR_arch_prctl)
-            // Not defined with every CPU architecture in Prod.
-            __NR_arch_prctl,
-#endif  // defined(__NR_arch_prctl)
-      })
-#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-    defined(THREAD_SANITIZER)
-      .AllowMmap()
+      .AddFile(NODE_PATH)
+      // Allow the getpid() syscall.
+      .AllowSyscall(__NR_getpid)
+
+// #ifdef __NR_access
+//       // On Debian, even static binaries check existence of /etc/ld.so.nohwcap.
+//       .BlockSyscallWithErrno(__NR_access, ENOENT)
+// #endif
+
+      // Examples for AddPolicyOnSyscall:
+      .AddPolicyOnSyscall(__NR_write,
+                          {
+                              // Load the first argument of write() (= fd)
+                              ARG_32(0),
+                              // Allow write(fd=STDOUT)
+                              JEQ32(1, ALLOW),
+                              // Allow write(fd=STDERR)
+                              JEQ32(2, ALLOW),
+                              JEQ32(3, ALLOW),
+                              JEQ32(4, ALLOW),
+                          })
+      // write() calls with fd not in (1, 2) will continue evaluating the
+      // policy. This means that other rules might still allow them.
+
+      // Allow exit() only with an exit_code of 0.
+      // Explicitly jumping to KILL, thus the following rules can not
+      // override this rule.
+      .AddPolicyOnSyscall(
+          __NR_exit_group,
+          {// Load first argument (exit_code).
+           ARG_32(0),
+           // Deny every argument except 0.
+           JNE32(0, KILL),
+           // Allow all exit() calls that were not previously forbidden
+           // = exit_code == 0.
+           ALLOW})
+
+      // = This won't have any effect as we handled every case of this syscall
+      // in the previous rule.
+      .AllowSyscall(__NR_exit_group)
+
+#ifdef __NR_open
+      .BlockSyscallWithErrno(__NR_open, ENOENT)
+#else
+      .BlockSyscallWithErrno(__NR_openat, ENOENT)
 #endif
       .BuildOrDie();
-}
-
-bool RunNodeScript(sandbox2::Comms* comms, std::string script, std::string* response) {
-  const uint8_t* buf = reinterpret_cast<const uint8_t*>(script.data());
-  size_t buf_size = script.size();
-
-  if (!comms->SendBytes(buf, buf_size)) {
-    LOG(ERROR) << "RunNodeScript_comms->SendBytes() failed";
-    return false;
-  }
-
-  if (!comms->RecvString(response)) {
-    LOG(ERROR) << "RunNodeScript_comms->RecvString failed";
-    return false;
-  }
-  return true;
 }
 
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
 
-  const std::string path = "/usr/bin/node";
-  std::vector<std::string> args = {path, "/usr/local/google/home/eliaskassell/Documents/github/dataform/tmp.js"};
-  std::vector<std::string> envs = {};
-  auto executor = absl::make_unique<sandbox2::Executor>(path, args, envs);
+  std::vector<std::string> args = {
+        NODE_PATH,
+      //   "/usr/local/google/home/eliaskassell/Documents/github/dataform/tmp.js"
+      };
+  auto executor = absl::make_unique<sandbox2::Executor>(NODE_PATH, args);
   
-  executor->set_enable_sandbox_before_exec(true);
+  executor
+    // Sandboxing is enabled by the sandbox itself. The sandboxed binary is
+    // not aware that it'll be sandboxed.
+    // Note: 'true' is the default setting for this class.
+    ->set_enable_sandbox_before_exec(true)
+    .limits()
+    // Remove restrictions on the size of address-space of sandboxed
+    // processes.
+    ->set_rlimit_as(RLIM64_INFINITY)
+    // Kill sandboxed processes with a signal (SIGXFSZ) if it writes more than
+    // these many bytes to the file-system.
+    .set_rlimit_fsize(1024 * 1024)
+    // The CPU time limit.
+    .set_rlimit_cpu(60)
+    .set_walltime_limit(absl::Seconds(30));
 
-  auto* comms = executor->ipc()->comms();
+  int proc_version_fd = open("/proc/version", O_RDONLY);
+  printf("Proc version: %i", proc_version_fd);
+  PCHECK(proc_version_fd != -1);
+
+  // Map this file's to sandboxee's stdin.
+  executor->ipc()->MapFd(proc_version_fd, STDIN_FILENO);
+
   auto policy = GetPolicy();
-
   sandbox2::Sandbox2 s2(std::move(executor), std::move(policy));
 
-  // Let the sandboxee run.
-  if (!s2.RunAsync()) {
-    auto result = s2.AwaitResult();
-    LOG(ERROR) << "RunAsync failed: " << result.ToString();
-    return 2;
-  }
-  
-  std::string script = "console.log('HeLlO tHeRe')";
-  std::string response;
+  printf("Running sandbox");
+  auto result = s2.Run();
+  printf("Run complete");
 
-  // if (!RunNodeScript(comms, script, &response)) {
-  //   LOG(ERROR) << "Sending failed";
-  //   if (!s2.IsTerminated()) {
-  //     // Kill the sandboxee, because failure to receive the data over the Comms
-  //     // channel doesn't automatically mean that the sandboxee itself had
-  //     // already finished. The final reason will not be overwritten, so if
-  //     // sandboxee finished because of e.g. timeout, the TIMEOUT reason will be
-  //     // reported.
-  //     LOG(INFO) << "Killing sandboxee";
-  //     s2.Kill();
-  //   }
-  // }
+  LOG(INFO) << "Final execution status: " << result.ToString();
 
-  auto result = s2.AwaitResult();
-  if (result.final_status() != sandbox2::Result::OK) {
-    LOG(ERROR) << "Sandbox error: " << result.ToString();
-    return 3;  // e.g. sandbox violation, signal (sigsegv)
-  }
-  auto code = result.reason_code();
-  if (code) {
-    LOG(ERROR) << "Sandboxee exited with non-zero: " << code;
-    return 4;  // e.g. normal child error
-  }
-  LOG(INFO) << "Sandboxee finished: " << result.ToString();
-  printf("RESPONSE: %s\n", response.c_str());
-  return EXIT_SUCCESS;
+  return result.final_status() == sandbox2::Result::OK ? EXIT_SUCCESS
+                                                       : EXIT_FAILURE;
 }
