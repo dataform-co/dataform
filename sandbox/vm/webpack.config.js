88a9a42b67d24457683a07a940639a7927cbae93
Elias Kassell <elias@dataform.co>
2021-02-01 16:26:40 +0000

    Revert to webpack, fix version bump

--- sandbox/vm/webpack.config.js
+++ sandbox/vm/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = (env, argv) => {
   const config = {
     mode: argv.mode || "development",
     entry: [path.resolve(process.env.RUNFILES, "df/sandbox/vm/worker")],
     output: {
-      path: path.dirname(path.resolve("sandbox/vm/worker_bundle.js")),
-      filename: path.basename("sandbox/vm/worker_bundle.js")
+      path: path.dirname(path.resolve(argv.outputPath)),
+      filename: path.basename(argv.outputPath)
     },
     target: "node",
     optimization: {
@@ -19,14 +20,22 @@ module.exports = (env, argv) => {
     //   fs: "empty",
     //   child_process: "empty"
     // },
+    node: {
+      __dirname: true
+    },
     resolve: {
       extensions: [".ts", ".js"],
       alias: {
         df: path.resolve(process.env.RUNFILES, "df")
       }
     },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ],
     externals: {
-      // vm2: "node-modules/vm2"
+      // vm2: "require('vm2')"
     }
   };
   return config;
