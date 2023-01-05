f61bcedfd4d84f667a711c414fa868eafa8bcc07
Elias Kassell <elias@dataform.co>
2023-01-05 15:45:12 +0000

    Environments and schedules (#1418)

--- core/targets.ts
+++ core/targets.ts
@@ -15,15 +15,13 @@ export function targetsAreEqual(a: dataform.ITarget, b: dataform.ITarget) {
 
 /**
  * Provides a readable string representation of the target which is used for e.g. specifying
- * actions on the CLI or as part of schedule configs.
+ * actions on the CLI.
  * This is effectively equivelant to an action "name".
- * 
+ *
  * This is an ambiguous transformation, multiple targets may map to the same string
  * and it should not be used for indexing. Use {@code targetStringifier} instead.
  */
-export function targetAsReadableString(
-  target: dataform.ITarget
-): string {
+export function targetAsReadableString(target: dataform.ITarget): string {
   const nameParts = [target.name, target.schema];
   if (!!target.database) {
     nameParts.push(target.database);
