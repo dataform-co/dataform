2ba748cae130657fa82c59a924c5a544e41d15ba
Elias Kassell <elias@dataform.co>
2023-08-14 17:28:00 +0100

    Make lexer typings safe (#1522)
    * Fix lexer typings
    
    * Simplify

--- sqlx/lexer.ts
+++ sqlx/lexer.ts
@@ -184,8 +184,11 @@ export class SyntaxTreeNode {
     const nodeStack = [currentNode];
     lexer.reset(code);
     for (const token of lexer) {
+      if (!token.type) {
+        continue;
+      }
       if (START_TOKEN_NODE_MAPPINGS.has(token.type)) {
-        const childType = START_TOKEN_NODE_MAPPINGS.get(token.type);
+        const childType = START_TOKEN_NODE_MAPPINGS.get(token.type)!;
         if (childType === SyntaxTreeNodeType.SQL && currentNode.type !== SyntaxTreeNodeType.SQL) {
           throw new Error("SQL syntax tree nodes may only be children of other SQL nodes.");
         }
@@ -199,7 +202,7 @@ export class SyntaxTreeNode {
         currentNode = nodeStack[nodeStack.length - 1];
       } else if (WHOLE_TOKEN_NODE_MAPPINGS.has(token.type)) {
         currentNode.push(
-          new SyntaxTreeNode(WHOLE_TOKEN_NODE_MAPPINGS.get(token.type)).push(token.value)
+          new SyntaxTreeNode(WHOLE_TOKEN_NODE_MAPPINGS.get(token.type)!).push(token.value)
         );
       } else {
         currentNode.push(token.value);
@@ -226,7 +229,7 @@ export class SyntaxTreeNode {
     return this.allChildren
       .map(child => {
         if (typeof child === "string") {
-          return mutator(child);
+          return mutator?.(child) || "";
         }
         return child.concatenate();
       })
@@ -241,7 +244,7 @@ export class SyntaxTreeNode {
     ) {
       this.allChildren[this.allChildren.length - 1] =
         this.allChildren[this.allChildren.length - 1] + child;
-      return;
+      return this;
     }
     this.allChildren.push(child);
     return this;
