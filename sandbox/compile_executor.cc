f011c8bf1e5e5d5af7731176a0b78b87c3b092a3
Lewis Hemens <lewishemens@google.com>
2023-09-04 14:35:58 +0000

    More dfco compatibility fixes

--- sandbox/compile_executor.cc
+++ sandbox/compile_executor.cc
@@ -55,21 +55,6 @@ namespace fs = std::filesystem;
 
 const int TIMEOUT_SECS = 1000;
 
-void OutputFD(int stdoutFd, int errFd)
-{
-    for (;;)
-    {
-        char stdoutBuf[4096];
-        char stderrBuf[4096];
-        ssize_t stdoutRLen = read(stdoutFd, stdoutBuf, sizeof(stdoutBuf));
-        ssize_t stderrRLen = read(errFd, stderrBuf, sizeof(stderrBuf));
-        if (stdoutRLen < 1)
-        {
-            break;
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -225,7 +210,7 @@ int main(int argc, char **argv)
                       .AllowSyscall(__NR_pipe2)
                       .AllowSyscall(__NR_eventfd2)
 
-                      .AllowSyscall(__NR_clone3)
+                      .AllowSyscall(435) // clone3
                       .AllowSyscall(__NR_sysinfo)
                       .AllowSyscall(__NR_statx)
                       .AllowSyscall(__NR_getcwd)
@@ -244,8 +229,6 @@ int main(int argc, char **argv)
 
     auto result = s2.AwaitResultWithTimeout(
         absl::Seconds(TIMEOUT_SECS));
-
-    OutputFD(stdoutFd, stderrFd);
 
     printf("Final execution status: %s\n", result->ToString().c_str());
 
