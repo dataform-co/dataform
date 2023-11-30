54108dc4a8eba8894ec840f5594660fc1d9d08d6
Elias Kassell <elias@dataform.co>
2023-11-30 11:02:03 +0000

    Tidy

--- core/main_test.ts
+++ core/main_test.ts
@@ -26,134 +26,134 @@ const EMPTY_NOTEBOOK_CONTENTS = '{ "cells": [] }';
 suite("@dataform/core", ({ afterEach }) => {
   const tmpDirFixture = new TmpDirFixture(afterEach);
 
-  // suite("workflow settings", () => {
-  //   test(`main succeeds when a valid workflow_settings.yaml is present`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(
-  //       path.join(projectDir, "workflow_settings.yaml"),
-  //       VALID_WORKFLOW_SETTINGS_YAML
-  //     );
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+  suite("workflow settings", () => {
+    test(`main succeeds when a valid workflow_settings.yaml is present`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(
+        path.join(projectDir, "workflow_settings.yaml"),
+        VALID_WORKFLOW_SETTINGS_YAML
+      );
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     const result = runMainInVm(coreExecutionRequest);
+      const result = runMainInVm(coreExecutionRequest);
 
-  //     expect(asPlainObject(result.compile.compiledGraph.projectConfig)).deep.equals(
-  //       asPlainObject({
-  //         warehouse: "bigquery",
-  //         defaultDatabase: "dataform"
-  //       })
-  //     );
-  //   });
+      expect(asPlainObject(result.compile.compiledGraph.projectConfig)).deep.equals(
+        asPlainObject({
+          warehouse: "bigquery",
+          defaultDatabase: "dataform"
+        })
+      );
+    });
 
-  //   // dataform.json for workflow settings is deprecated, but still currently supported.
-  //   test(`main succeeds when a valid dataform.json is present`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(path.join(projectDir, "dataform.json"), VALID_DATAFORM_JSON);
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    // dataform.json for workflow settings is deprecated, but still currently supported.
+    test(`main succeeds when a valid dataform.json is present`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(path.join(projectDir, "dataform.json"), VALID_DATAFORM_JSON);
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     const result = runMainInVm(coreExecutionRequest);
+      const result = runMainInVm(coreExecutionRequest);
 
-  //     expect(asPlainObject(result.compile.compiledGraph.projectConfig)).deep.equals(
-  //       asPlainObject({
-  //         warehouse: "bigquery",
-  //         defaultDatabase: "dataform"
-  //       })
-  //     );
-  //   });
+      expect(asPlainObject(result.compile.compiledGraph.projectConfig)).deep.equals(
+        asPlainObject({
+          warehouse: "bigquery",
+          defaultDatabase: "dataform"
+        })
+      );
+    });
 
-  //   test(`main fails when no workflow settings file is present`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when no workflow settings file is present`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw(
-  //       "Failed to resolve workflow_settings.yaml"
-  //     );
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw(
+        "Failed to resolve workflow_settings.yaml"
+      );
+    });
 
-  //   test(`main fails when both workflow settings and dataform.json files are present`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(path.join(projectDir, "dataform.json"), VALID_DATAFORM_JSON);
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(
-  //       path.join(projectDir, "workflow_settings.yaml"),
-  //       VALID_WORKFLOW_SETTINGS_YAML
-  //     );
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when both workflow settings and dataform.json files are present`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(path.join(projectDir, "dataform.json"), VALID_DATAFORM_JSON);
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(
+        path.join(projectDir, "workflow_settings.yaml"),
+        VALID_WORKFLOW_SETTINGS_YAML
+      );
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw(
-  //       "dataform.json has been deprecated and cannot be defined alongside workflow_settings.yaml"
-  //     );
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw(
+        "dataform.json has been deprecated and cannot be defined alongside workflow_settings.yaml"
+      );
+    });
 
-  //   test(`main fails when workflow_settings.yaml is an invalid yaml file`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(path.join(projectDir, "workflow_settings.yaml"), "&*19132sdS:asd:");
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when workflow_settings.yaml is an invalid yaml file`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(path.join(projectDir, "workflow_settings.yaml"), "&*19132sdS:asd:");
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw("workflow_settings.yaml is invalid");
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw("workflow_settings.yaml is invalid");
+    });
 
-  //   test(`main fails when dataform.json is an invalid json file`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(path.join(projectDir, "dataform.json"), '{keyWithNoQuotes: "validValue"}');
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when dataform.json is an invalid json file`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(path.join(projectDir, "dataform.json"), '{keyWithNoQuotes: "validValue"}');
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw(
-  //       "Unexpected token k in JSON at position 1"
-  //     );
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw(
+        "Unexpected token k in JSON at position 1"
+      );
+    });
 
-  //   test(`main fails when a valid workflow_settings.yaml contains unknown fields`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(
-  //       path.join(projectDir, "workflow_settings.yaml"),
-  //       "notAProjectConfigField: value"
-  //     );
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when a valid workflow_settings.yaml contains unknown fields`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(
+        path.join(projectDir, "workflow_settings.yaml"),
+        "notAProjectConfigField: value"
+      );
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw(
-  //       "Workflow settings error: unexpected key 'notAProjectConfigField'"
-  //     );
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw(
+        "Workflow settings error: unexpected key 'notAProjectConfigField'"
+      );
+    });
 
-  //   test(`main fails when a valid dataform.json contains unknown fields`, () => {
-  //     const projectDir = tmpDirFixture.createNewTmpDir();
-  //     // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
-  //     fs.writeFileSync(
-  //       path.join(projectDir, "dataform.json"),
-  //       `{"notAProjectConfigField": "value"}`
-  //     );
-  //     const coreExecutionRequest = dataform.CoreExecutionRequest.create({
-  //       compile: { compileConfig: { projectDir } }
-  //     });
+    test(`main fails when a valid dataform.json contains unknown fields`, () => {
+      const projectDir = tmpDirFixture.createNewTmpDir();
+      // tslint:disable-next-line: tsr-detect-non-literal-fs-filename
+      fs.writeFileSync(
+        path.join(projectDir, "dataform.json"),
+        `{"notAProjectConfigField": "value"}`
+      );
+      const coreExecutionRequest = dataform.CoreExecutionRequest.create({
+        compile: { compileConfig: { projectDir } }
+      });
 
-  //     expect(() => runMainInVm(coreExecutionRequest)).to.throw(
-  //       "Workflow settings error: unexpected key 'notAProjectConfigField'"
-  //     );
-  //   });
+      expect(() => runMainInVm(coreExecutionRequest)).to.throw(
+        "Workflow settings error: unexpected key 'notAProjectConfigField'"
+      );
+    });
 
-  //   // TODO(ekrekr): add a test for nested fields, once they exist.
-  // });
+    // TODO(ekrekr): add a test for nested fields, once they exist.
+  });
 
   suite("notebooks", () => {
     test(`notebooks can be loaded via an actions config file`, () => {
