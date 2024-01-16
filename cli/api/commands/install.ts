bc49654c49a4d96eddcfc9705e68ae72de473f6c
Elias Kassell <elias@dataform.co>
2024-01-16 13:45:34 +0000

    Update Dataform CLI npm installs to best-effort (#1649)
    * Update Dataform CLI npm installs to best-effort
    
    * Tidy logic
    
    * Prevent errors when package.json already present
    
    * Fix lint

--- cli/api/commands/install.ts
+++ cli/api/commands/install.ts
@@ -1,11 +1,55 @@
+import * as fs from "fs";
+import { load as loadYaml, YAMLException } from "js-yaml";
 import * as path from "path";
 import { promisify } from "util";
 
 import * as childProcess from "child_process";
+import { dataform } from "df/protos/ts";
 
-export async function install(projectDir: string, skipInstall?: boolean) {
+export async function install(projectPath: string, skipInstall?: boolean) {
   if (skipInstall) {
     return;
   }
-  await promisify(childProcess.exec)("npm i --ignore-scripts", { cwd: path.resolve(projectDir) });
+  const resolvedProjectPath = path.resolve(projectPath);
+  const workflowSettingsPath = path.join(resolvedProjectPath, "workflow_settings.yaml");
+  const packageJsonPath = path.join(resolvedProjectPath, "package.json");
+  const packageLockJsonPath = path.join(resolvedProjectPath, "package-lock.json");
+
+  let installCommand = "npm i --ignore-scripts";
+
+  // Core's readWorkflowSettings method cannot be used for this because Core assumes that
+  // `require` can read YAML files directly.
+  const dataformCoreVersion = readDataformCoreVersionIfPresent(workflowSettingsPath);
+
+  if (dataformCoreVersion) {
+    // If there are other packages already in the package.json, specifying a specific package to
+    // install will trigger the other packages to be installed too.
+    installCommand += ` @dataform/core@${dataformCoreVersion}`;
+  }
+
+  if (!dataformCoreVersion && !fs.existsSync(packageJsonPath)) {
+    throw new Error(
+      "dataformCoreVersion must be specified either in workflow_settings.yaml or via a package.json"
+    );
+  }
+
+  await promisify(childProcess.exec)(installCommand, { cwd: resolvedProjectPath });
+}
+
+function readDataformCoreVersionIfPresent(workflowSettingsPath: string): string {
+  if (!fs.existsSync(workflowSettingsPath)) {
+    return "";
+  }
+
+  const workflowSettingsContent = fs.readFileSync(workflowSettingsPath, "utf-8");
+  let workflowSettingsAsJson = {};
+  try {
+    workflowSettingsAsJson = loadYaml(workflowSettingsContent);
+  } catch (e) {
+    if (e instanceof YAMLException) {
+      throw Error(`${path} is not a valid YAML file: ${e}`);
+    }
+    throw e;
+  }
+  return dataform.ProjectConfig.create(workflowSettingsAsJson).dataformCoreVersion;
 }
