4015b05d85edd03acfcd4d92ccf4eff94e63bd4e
moker-spaghetti <32996519+moker-spaghetti@users.noreply.github.com>
2024-07-08 23:32:18 +0900

    Add workflow_settings.yaml validation/completion VSCode extension (#1770)
    * add workflow_settings.yaml validation to vscode extension
    
    * fix: shows errors on unknown properties

--- vscode/extension.ts
+++ vscode/extension.ts
@@ -56,4 +56,35 @@ export async function activate(context: vscode.ExtensionContext) {
   client.onNotification("success", message => {
     vscode.window.showInformationMessage(message);
   });
+
+  // Recommend YAML extension if not installed
+  // We also can add the extension to "extensionDependencies" in package.json,
+  // but this way we can avoid forcing users to install the extension.
+  // You can control this recommendation behavior through the setting.
+  if (workspace.getConfiguration("dataform").get("recommendYamlExtension")) {
+    const yamlExtension = vscode.extensions.getExtension("redhat.vscode-yaml");
+    if (!yamlExtension) {
+      await vscode.window.showInformationMessage(
+        "The Dataform extension recommends installing the YAML extension for workflow_settings.yaml support.",
+        "Install",
+        "Don't show again"
+      ).then(selection => {
+        if (selection === "Install") {
+          // Open the YAML extension page
+          vscode.env.openExternal(
+            vscode.Uri.parse(
+              "vscode:extension/redhat.vscode-yaml"
+            )
+          );
+        } else if (selection === "Don't show again") {
+          // Disable the recommendation
+          workspace.getConfiguration("dataform").update(
+            "recommendYamlExtension",
+            false,
+            vscode.ConfigurationTarget.Global
+          );
+        }
+      });
+    }
+  }
 }
