d0393150991516b0c590a6c8e05197d319243f5d
Elias Kassell <eliaskassell@gmail.com>
2024-09-09 11:13:48 +0100

    Revert "fix: dry run fails when table/view/assertion already does not exsist …"
    This reverts commit 4d209298c9aa197acca34bed799d175e67433ec2.

--- cli/api/commands/run.ts
+++ cli/api/commands/run.ts
@@ -365,7 +365,6 @@ export class Runner {
       // (i.e. it must still be RUNNING, and not FAILED).
       actionResult.status === dataform.ActionResult.ExecutionStatus.RUNNING &&
       !(this.graph.runConfig && this.graph.runConfig.disableSetMetadata) &&
-      !(this.executionOptions?.bigquery?.dryRun) &&
       action.type === "table"
     ) {
       try {
