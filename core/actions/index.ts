f4b021ceeff2eae952e70d240cde890edf3fe16c
Elias Kassell <elias@dataform.co>
2024-12-13 14:44:06 +0000

    Fix string value for uniqueKey assertions, tidy tests

--- core/actions/index.ts
+++ core/actions/index.ts
@@ -1,11 +1,11 @@
 import { Assertion } from "df/core/actions/assertion";
 import { DataPreparation } from "df/core/actions/data_preparation";
 import { Declaration } from "df/core/actions/declaration";
-import { IncrementalTable } from "df/core/actions/incremental_table";
+import { IncrementalTable, ILegacyIncrementalTableConfig } from "df/core/actions/incremental_table";
 import { Notebook } from "df/core/actions/notebook";
 import { Operation } from "df/core/actions/operation";
 import { Table } from "df/core/actions/table";
-import { View } from "df/core/actions/view";
+import { View, ILegacyViewConfig } from "df/core/actions/view";
 import { Session } from "df/core/session";
 import { dataform } from "df/protos/ts";
 
@@ -93,5 +93,30 @@ export abstract class ActionBuilder<T> {
         target
       );
     }
+  }
+}
+
+export class LegacyConfigConverter {
+  public static insertLegacyInlineAssertionsToConfigProto<
+    T extends ILegacyIncrementalTableConfig | ILegacyViewConfig
+  >(legacyConfig: T): T {
+    if (legacyConfig?.assertions) {
+      if (!!legacyConfig.assertions.uniqueKey) {
+        if (typeof legacyConfig.assertions.uniqueKey === "string") {
+          legacyConfig.assertions.uniqueKey = [legacyConfig.assertions.uniqueKey];
+        }
+      }
+      // This determines if the uniqueKeys is of the legacy type.
+      if (legacyConfig.assertions.uniqueKeys?.[0]?.length > 0) {
+        legacyConfig.assertions.uniqueKeys = (legacyConfig.assertions
+          .uniqueKeys as string[][]).map(uniqueKey =>
+          dataform.ActionConfig.TableAssertionsConfig.UniqueKey.create({ uniqueKey })
+        );
+      }
+      if (typeof legacyConfig.assertions.nonNull === "string") {
+        legacyConfig.assertions.nonNull = [legacyConfig.assertions.nonNull];
+      }
+    }
+    return legacyConfig;
   }
 }
