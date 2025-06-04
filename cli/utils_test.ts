e9c3e8ead35e52e2d0404f7f80142197aa22e93a
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-06-04 14:34:19 +0100

    chore: remove unnecessary new line

--- cli/utils_test.ts
+++ cli/utils_test.ts
@@ -10,6 +10,5 @@ suite('format execution suffix', () => {
         expect(formatExecutionSuffix([], ["10 MiB"])).deep.equals(" (Bytes billed: 10 MiB)");
         expect(formatExecutionSuffix(["dataform-915a03fe1"], ["17 KiB"])).deep.equals(" (jobId: dataform-915a03fe1 | Bytes billed: 17 KiB)");
         expect(formatExecutionSuffix(["dataform-915a03fe1", "dataform-915a03fe2"], ["17 KiB", "1 GiB"])).deep.equals(" (jobId: dataform-915a03fe1, dataform-915a03fe2 | Bytes billed: 17 KiB, 1 GiB)");
-
     });
 });
