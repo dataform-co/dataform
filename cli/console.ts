7ee9c7031920639a7647d57889af5408c4a17237
ashish.alex10@gmail.com <ashish.alex10@gmail.com>
2025-06-05 18:51:17 +0100

    feat: more comprehensive comment to describe the function

--- cli/console.ts
+++ cli/console.ts
@@ -94,7 +94,10 @@ export function print(text: string) {
 
 export function formatStackTraceForQuietCompilation(compileError: dataform.ICompilationError): string {
     // Show only first 3 or available lines for cleaner error output
-    // which contains the information on the file where the error occurred and the additional information on the error sufficient for the user to fix the error
+    // which contains the information on the file where the error occurred and the sufficient metadata for the user to fix the error. For e.g.
+    // (line: 1) Unexpected identifier <file_path>: <line_number>
+    // (line: 2) tags: ["<tag_name>"]
+    // (line: 3) ^^^^
     if (compileError?.message?.includes("Unexpected identifier")) {
       if (!compileError.stack) {
         return "";
