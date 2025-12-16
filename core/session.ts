9764de00f590c483b6d25efc72346df4cede3467
Fernando Velasquez <fevelasquez@google.com>
2025-12-16 18:47:38 -0500

    Added new properties to Test proto

--- core/session.ts
+++ core/session.ts
@@ -52,8 +52,8 @@ export class Session {
   public canonicalProjectConfig: dataform.ProjectConfig;
 
   public actions: Action[];
+  public tests: Test[];
   public indexedActions: ResolvableMap<Action>;
-  public tests: { [name: string]: Test };
 
   // This map holds information about what assertions are dependent
   // upon a certain action in our actions list. We use this later to resolve dependencies.
@@ -83,7 +83,7 @@ export class Session {
       dataform.ProjectConfig.create(originalProjectConfig || projectConfig || DEFAULT_CONFIG)
     );
     this.actions = [];
-    this.tests = {};
+    this.tests = [];
     this.graphErrors = { compilationErrors: [] };
     this.jitContextData = new google.protobuf.Struct();
   }
@@ -421,7 +421,8 @@ export class Session {
     newTest.session = this;
     newTest.setFilename(utils.getCallerFile(this.rootDir));
     // Add it to global index.
-    this.tests[name] = newTest;
+    this.actions.push(newTest)
+    this.tests.push(newTest);
     return newTest;
   }
 
@@ -471,7 +472,6 @@ export class Session {
     );
 
     // defaultLocation is no longer a required parameter to support location auto-selection.
-
     if (
       !!this.projectConfig.vars &&
       !Object.values(this.projectConfig.vars).every(value => typeof value === "string")
@@ -496,7 +496,9 @@ export class Session {
       declarations: this.compileGraphChunk(
         this.actions.filter(action => action instanceof Declaration)
       ),
-      tests: this.compileGraphChunk(Object.values(this.tests)),
+      tests: this.compileGraphChunk(
+        this.actions.filter(action => action instanceof Test)
+      ),
       notebooks: this.compileGraphChunk(this.actions.filter(action => action instanceof Notebook)),
       dataPreparations: this.compileGraphChunk(
         this.actions.filter(action => action instanceof DataPreparation)
@@ -507,13 +509,26 @@ export class Session {
       jitData: this.jitContextData,
     });
 
+    // Add unit tests as dependencies to the parent actions
+    this.actions
+      .filter(action => action instanceof Test)
+      .map(test => test as Test)
+      .forEach(test => {
+        this.indexedActions
+          .find(test.getTestTarget())
+          .filter(action => action instanceof Table || action instanceof View)
+          .map(action => action as Table | View)
+          .forEach(tableOrViewAction => tableOrViewAction.dependencies(utils.resolvableAsTarget(test.getTarget())));
+      });
+
     this.fullyQualifyDependencies(
       [].concat(
         compiledGraph.tables,
         compiledGraph.assertions,
         compiledGraph.operations,
         compiledGraph.notebooks,
-        compiledGraph.dataPreparations
+        compiledGraph.dataPreparations,
+        compiledGraph.tests
       )
     );
 
@@ -523,7 +538,8 @@ export class Session {
         compiledGraph.assertions,
         compiledGraph.operations,
         compiledGraph.notebooks,
-        compiledGraph.dataPreparations
+        compiledGraph.dataPreparations,
+        compiledGraph.tests
       ),
       [].concat(compiledGraph.declarations.map(declaration => declaration.target))
     );
@@ -538,7 +554,8 @@ export class Session {
         compiledGraph.assertions,
         compiledGraph.operations,
         compiledGraph.notebooks,
-        compiledGraph.dataPreparations
+        compiledGraph.dataPreparations,
+        compiledGraph.tests
       )
     );
     verifyObjectMatchesProto(
@@ -577,7 +594,7 @@ export class Session {
     return !!this.projectConfig.tablePrefix ? `${this.projectConfig.tablePrefix}_` : "";
   }
 
-  private compileGraphChunk<T>(actions: Array<Action | Test>): T[] {
+  private compileGraphChunk<T>(actions: Action[]): T[] {
     const compiledChunks: T[] = [];
 
     actions.forEach(action => {
@@ -596,7 +613,7 @@ export class Session {
     actions.forEach(action => {
       const fullyQualifiedDependencies: { [name: string]: dataform.ITarget } = {};
       if (action instanceof dataform.Declaration || !action.dependencyTargets) {
-        // Declarations cannot have dependencies.
+        // Declarations cannot have dependencies. 
         return;
       }
       for (const dependency of action.dependencyTargets) {
