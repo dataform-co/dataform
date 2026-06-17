b9ba4e28e85b18a415697ee3347c44ff981c4fd0
ikholopov <ikholopov@google.com>
2026-06-17 14:40:06 +0200

    improve patch

--- testing/resolver-patch.js
+++ testing/resolver-patch.js
@@ -2,12 +2,15 @@
   const Module = require('module');
   const path = require('path');
 
+  const RUNFILES_SUFFIX = '.runfiles';
+  const DF_PREFIX = 'df/';
+
   let runfilesDir = process.env.RUNFILES;
   if (!runfilesDir) {
     const mainFilename = require.main ? require.main.filename : __filename;
-    const runfilesIndex = mainFilename.indexOf('.runfiles');
+    const runfilesIndex = mainFilename.indexOf(RUNFILES_SUFFIX);
     if (runfilesIndex !== -1) {
-      runfilesDir = mainFilename.substring(0, runfilesIndex + 9);
+      runfilesDir = mainFilename.substring(0, runfilesIndex + RUNFILES_SUFFIX.length);
     }
   }
   
@@ -16,22 +19,29 @@
   }
   
   const originalResolveFilename = Module._resolveFilename;
-  
-  Module._resolveFilename = function (request, parent, isMain, options) {
-    if (request === 'df' || request.startsWith('df/')) {
-      if (runfilesDir) {
-        const relativePath = request === 'df' ? '' : request.substring(3);
-        const resolvedPath = path.join(runfilesDir, '_main', relativePath);
-        try {
-          return originalResolveFilename(resolvedPath, parent, isMain, options);
-        } catch (e) {
-          try {
-            return originalResolveFilename(path.join(runfilesDir, relativePath), parent, isMain, options);
-          } catch (e2) {
-            // ignore
-          }
-          throw e;
+
+  function tryResolve(paths, parent, isMain, options) {
+    let firstError;
+    for (const p of paths) {
+      try {
+        return originalResolveFilename(p, parent, isMain, options);
+      } catch (e) {
+        if (!firstError) {
+          firstError = e;
         }
+      }
+    }
+    throw firstError;
+  }
+
+  Module._resolveFilename = function (request, parent, isMain, options) {
+    if (request === 'df' || request.startsWith(DF_PREFIX)) {
+      if (runfilesDir) {
+        const relativePath = request === 'df' ? '' : request.substring(DF_PREFIX.length);
+        return tryResolve([
+          path.join(runfilesDir, '_main', relativePath),
+          path.join(runfilesDir, relativePath)
+        ], parent, isMain, options);
       }
     }
     try {
@@ -39,13 +49,12 @@
     } catch (err) {
       if (runfilesDir && !request.startsWith('.') && !path.isAbsolute(request)) {
         try {
-          return originalResolveFilename(path.join(runfilesDir, '_main', 'node_modules', request), parent, isMain, options);
+          return tryResolve([
+            path.join(runfilesDir, '_main', 'node_modules', request),
+            path.join(runfilesDir, 'node_modules', request)
+          ], parent, isMain, options);
         } catch (err2) {
-          try {
-            return originalResolveFilename(path.join(runfilesDir, 'node_modules', request), parent, isMain, options);
-          } catch (err3) {
-            // ignore
-          }
+          // ignore, throw original err
         }
       }
       throw err;
