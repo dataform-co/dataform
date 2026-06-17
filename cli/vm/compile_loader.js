79ca95f3943e09bfed6f0f82a4d6c8870b6392ff
ikholopov <ikholopov@google.com>
2026-06-17 14:30:27 +0200

    cleanup files

--- cli/vm/compile_loader.js
+++
@@ -1,7 +0,0 @@
-require('../../testing/resolver-patch.js');
-
-const compileModule = require('./compile.js');
-if (process.send) {
-  process.send({ type: "worker_booted" });
-}
-compileModule.listenForCompileRequest();
