dataform-co/dataform

commit 377c0c78f3511b2d615687327c16cf09f26fdbfc

Table configs as protos (#1780)
* Progress configuring table configs as protos

* tidy

* More type fixes, working test build, but not passing

* Fix unique key test

* Import fix

* More progress making the table conversion work

* Fix some default value population

* Fix regression for BigQuery error messages on duplicate keys

* Fix remaining tests

* Tidy

* comments

core/actions/index.ts
@@ -4,8 +4,9 @@ import { Declaration } from "df/core/actions/declaration";
 import { ILegacyIncrementalTableConfig, IncrementalTable } from "df/core/actions/incremental_table";
 import { Notebook } from "df/core/actions/notebook";
 import { Operation } from "df/core/actions/operation";
-import { Table } from "df/core/actions/table";
+import { ILegacyTableConfig, Table } from "df/core/actions/table";
 import { ILegacyViewConfig, View } from "df/core/actions/view";
+import { ICommonContext } from "df/core/common";
 import { Session } from "df/core/session";
 import { dataform } from "df/protos/ts";
 
@@ -96,15 +97,52 @@ export abstract class ActionBuilder<T> {
   }
 }
 
+/**
+ * Context methods are available when evaluating contextable SQL code, such as
+ * within SQLX files, or when using a [Contextable](#Contextable) argument with the JS API.
+ */
+export interface ITableContext extends ICommonContext {
+  /**
+   * Shorthand for an `if` condition. Equivalent to `cond ? trueCase : falseCase`.
+   * `falseCase` is optional, and defaults to an empty string.
+   */
+  when: (cond: boolean, trueCase: string, falseCase?: string) => string;
+
+  /**
+   * Indicates whether the config indicates the file is dealing with an incremental table.
+   */
+  incremental: () => boolean;
+}
+
 export class LegacyConfigConverter {
+  // This is a workaround to make bigquery options output empty fields with the same behaviour as
+  // they did previously.
+  public static legacyConvertBigQueryOptions(
+    bigquery: dataform.IBigQueryOptions
+  ): dataform.IBigQueryOptions {
+    let bigqueryFiltered: dataform.IBigQueryOptions = {};
+    Object.entries(bigquery).forEach(([key, value]) => {
+      if (Array.isArray(value) && value.length === 0) {
+        return;
+      } else if (typeof value === "object" && Object.entries(value).length === 0) {
+        return;
+      }
+      if (value) {
+        bigqueryFiltered = {
+          ...bigqueryFiltered,
+          [key]: value
+        };
+      }
+    });
+    return bigqueryFiltered;
+  }
+
   public static insertLegacyInlineAssertionsToConfigProto<
-    T extends ILegacyIncrementalTableConfig | ILegacyViewConfig
+    T extends ILegacyTableConfig | ILegacyIncrementalTableConfig | ILegacyViewConfig
   >(legacyConfig: T): T {
     if (legacyConfig?.assertions) {
-      if (!!legacyConfig.assertions.uniqueKey) {
-        if (typeof legacyConfig.assertions.uniqueKey === "string") {
-          legacyConfig.assertions.uniqueKey = [legacyConfig.assertions.uniqueKey];
-        }
+      if (typeof legacyConfig.assertions?.uniqueKey === "string") {
+        legacyConfig.assertions.uniqueKey = [legacyConfig.assertions.uniqueKey];
       }
       // This determines if the uniqueKeys is of the legacy type.
       if (legacyConfig.assertions.uniqueKeys?.[0]?.length > 0) {
@@ -119,4 +157,57 @@ export class LegacyConfigConverter {
     }
     return legacyConfig;
   }
+
+  public static insertLegacyBigQueryOptionsToConfigProto<
+    T extends ILegacyTableConfig | ILegacyIncrementalTableConfig
+  >(legacyConfig: T): T {
+    if (!legacyConfig?.bigquery) {
+      return legacyConfig;
+    }
+    if (!!legacyConfig.bigquery.partitionBy) {
+      legacyConfig.partitionBy = legacyConfig.bigquery.partitionBy;
+      delete legacyConfig.bigquery.partitionBy;
+    }
+    if (!!legacyConfig.bigquery.clusterBy) {
+      legacyConfig.clusterBy = legacyConfig.bigquery.clusterBy;
+      delete legacyConfig.bigquery.clusterBy;
+    }
+    if (!!legacyConfig.bigquery.updatePartitionFilter) {
+      (legacyConfig as ILegacyIncrementalTableConfig).updatePartitionFilter =
+        legacyConfig.bigquery.updatePartitionFilter;
+      delete legacyConfig.bigquery.updatePartitionFilter;
+    }
+    if (!!legacyConfig.bigquery.labels) {
+      legacyConfig.labels = legacyConfig.bigquery.labels;
+      delete legacyConfig.bigquery.labels;
+    }
+    if (!!legacyConfig.bigquery.partitionExpirationDays) {
+      legacyConfig.partitionExpirationDays = legacyConfig.bigquery.partitionExpirationDays;
+      delete legacyConfig.bigquery.partitionExpirationDays;
+    }
+    if (!!legacyConfig.bigquery.requirePartitionFilter) {
+      legacyConfig.requirePartitionFilter = legacyConfig.bigquery.requirePartitionFilter;
+      delete legacyConfig.bigquery.requirePartitionFilter;
+    }
+    if (!!legacyConfig.bigquery.additionalOptions) {
+      legacyConfig.additionalOptions = legacyConfig.bigquery.additionalOptions;
+      delete legacyConfig.bigquery.additionalOptions;
+    }
+    // To prevent skipping throwing an error when there are additional, unused fields, only delete
+    // the legacy bigquery object if there are no more fields left on it.
+    if (Object.keys(legacyConfig.bigquery).length === 0) {
+      delete legacyConfig.bigquery;
+    }
+    return legacyConfig;
+  }
+}
+
+export interface ILegacyTableBigqueryConfig {
+  partitionBy?: string;
+  clusterBy?: string[];
+  updatePartitionFilter?: string;
+  labels?: { [key: string]: string };
+  partitionExpirationDays?: number;
+  requirePartitionFilter?: boolean;
+  additionalOptions?: { [key: string]: string };
 }

cli/api/commands/run.ts
@@ -7,7 +7,6 @@ import { Flags } from "df/common/flags";
 import { retry } from "df/common/promises";
 import { deepClone, equals } from "df/common/protos";
 import { StringifiedMap, StringifiedSet } from "df/common/strings/stringifier";
-import { IBigQueryOptions } from "df/core/actions/table";
 import { targetStringifier } from "df/core/targets";
 import { dataform } from "df/protos/ts";
 
@@ -399,7 +398,7 @@ export class Runner {
     client: dbadapters.IDbClient,
     task: dataform.IExecutionTask,
     parentAction: dataform.IActionResult,
-    options: { bigquery?: IBigQueryOptions & IBigQueryExecutionOptions }
+    options: { bigquery?: dataform.IBigQueryOptions & IBigQueryExecutionOptions }
   ): Promise<dataform.TaskResult.ExecutionStatus> {
     const timer = Timer.start();
     const taskResult: dataform.ITaskResult = {

cli/index_test.ts
@@ -220,6 +220,7 @@ select 1 as \${dataform.projectConfig.vars.testVar2}
           query: "\n\nselect 1 as testValue2\n",
           disabled: false,
           fileName: "definitions/example.sqlx",
+          hermeticity: "NON_HERMETIC",
           tags: ["someTag"]
         }
       ],
@@ -268,7 +269,7 @@ select 1 as \${dataform.projectConfig.vars.testVar2}
       actions: [
         {
           fileName: "definitions/example.sqlx",
-          hermeticity: "HERMETIC",
+          hermeticity: "NON_HERMETIC",
           tableType: "table",
           target: {
             database: "dataform-open-source",

core/actions/incremental_table.ts
@@ -1,7 +1,11 @@
 import { verifyObjectMatchesProto, VerifyProtoErrorBehaviour } from "df/common/protos";
-import { ActionBuilder, LegacyConfigConverter } from "df/core/actions";
+import {
+  ActionBuilder,
+  ILegacyTableBigqueryConfig,
+  ITableContext,
+  LegacyConfigConverter
+} from "df/core/actions";
 import { Assertion } from "df/core/actions/assertion";
-import { ITableContext, Table } from "df/core/actions/table";
 import { ColumnDescriptors } from "df/core/column_descriptors";
 import { Contextable, Resolvable } from "df/core/common";
 import * as Path from "df/core/path";
@@ -9,11 +13,13 @@ import { Session } from "df/core/session";
 import {
   actionConfigToCompiledGraphTarget,
   addDependenciesToActionDependencyTargets,
+  checkExcessProperties,
   configTargetToCompiledGraphTarget,
   nativeRequire,
   resolvableAsTarget,
   resolveActionsConfigFilename,
   setNameAndTarget,
+  strictKeysOf,
   toResolvable,
   validateQueryString
 } from "df/core/utils";
@@ -31,21 +37,11 @@ export interface ILegacyIncrementalTableConfig
   schema: string;
   fileName: string;
   type: string;
-  bigquery?: ILegacyIncrementalTableBigqueryConfig;
+  bigquery?: ILegacyTableBigqueryConfig;
   // Legacy incremental table config's table assertions cannot directly extend the protobuf
   // incremental table config definition because of legacy incremental table config's flexible
   // types.
   assertions: any;
-}
-
-interface ILegacyIncrementalTableBigqueryConfig {
-  partitionBy?: string;
-  clusterBy?: string[];
-  updatePartitionFilter?: string;
-  labels?: { [key: string]: string };
-  partitionExpirationDays?: number;
-  requirePartitionFilter?: boolean;
-  additionalOptions?: { [key: string]: string };
 }
 
 /**
@@ -117,9 +113,6 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
     }
     if (config.disabled) {
       this.disabled();
-    }
-    if (Object.keys(config.additionalOptions).length > 0) {
-      this.bigquery({ additionalOptions: config.additionalOptions });
     }
     if (config.tags) {
       this.tags(config.tags);
@@ -196,12 +189,8 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
     return this;
   }
 
-  public protected(defaultsToTrueProtected: boolean) {
-    // To prevent accidental data deletion, protected defaults to true if unspecified.
-    if (defaultsToTrueProtected === undefined || defaultsToTrueProtected === null) {
-      defaultsToTrueProtected = true;
-    }
-    this.proto.protected = defaultsToTrueProtected;
+  public protected(isProtected: boolean) {
+    this.proto.protected = isProtected;
     return this;
   }
 
@@ -217,16 +206,7 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
       this.proto.actionDescriptor.bigqueryLabels = bigquery.labels;
     }
 
-    // Remove all falsy values, to preserve backwards compatability of compiled graph output.
-    let bigqueryFiltered: dataform.IBigQueryOptions = {};
-    Object.entries(bigquery).forEach(([key, value]) => {
-      if (value) {
-        bigqueryFiltered = {
-          ...bigqueryFiltered,
-          [key]: value
-        };
-      }
-    });
+    const bigqueryFiltered = LegacyConfigConverter.legacyConvertBigQueryOptions(bigquery);
     if (Object.values(bigqueryFiltered).length > 0) {
       this.proto.bigquery = dataform.BigQueryOptions.create(bigqueryFiltered);
     }
@@ -379,15 +359,10 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
 
     this.proto.query = context.apply(this.contextableQuery);
 
-    if (this.proto.enumType === dataform.TableType.INCREMENTAL) {
-      this.proto.incrementalQuery = incrementalContext.apply(this.contextableQuery);
+    this.proto.incrementalQuery = incrementalContext.apply(this.contextableQuery);
 
-      this.proto.incrementalPreOps = this.contextifyOps(this.contextablePreOps, incrementalContext);
-      this.proto.incrementalPostOps = this.contextifyOps(
-        this.contextablePostOps,
-        incrementalContext
-      );
-    }
+    this.proto.incrementalPreOps = this.contextifyOps(this.contextablePreOps, incrementalContext);
+    this.proto.incrementalPostOps = this.contextifyOps(this.contextablePostOps, incrementalContext);
 
     if (this.contextableWhere) {
       this.proto.where = context.apply(this.contextableWhere);
@@ -456,31 +431,31 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
       unverifiedConfig = LegacyConfigConverter.insertLegacyInlineAssertionsToConfigProto(
         unverifiedConfig
       );
-      if (unverifiedConfig?.bigquery) {
-        if (!!unverifiedConfig.bigquery.partitionBy) {
-          unverifiedConfig.partitionBy = unverifiedConfig.bigquery.partitionBy;
-        }
-        if (!!unverifiedConfig.bigquery.clusterBy) {
-          unverifiedConfig.clusterBy = unverifiedConfig.bigquery.clusterBy;
-        }
-        if (!!unverifiedConfig.bigquery.updatePartitionFilter) {
-          unverifiedConfig.updatePartitionFilter = unverifiedConfig.bigquery.updatePartitionFilter;
-        }
-        if (!!unverifiedConfig.bigquery.labels) {
-          unverifiedConfig.labels = unverifiedConfig.bigquery.labels;
-        }
-        if (!!unverifiedConfig.bigquery.partitionExpirationDays) {
-          unverifiedConfig.partitionExpirationDays =
-            unverifiedConfig.bigquery.partitionExpirationDays;
-        }
-        if (!!unverifiedConfig.bigquery.requirePartitionFilter) {
-          unverifiedConfig.requirePartitionFilter =
-            unverifiedConfig.bigquery.requirePartitionFilter;
-        }
-        if (!!unverifiedConfig.bigquery.additionalOptions) {
-          unverifiedConfig.additionalOptions = unverifiedConfig.bigquery.additionalOptions;
-        }
-        delete unverifiedConfig.bigquery;
+      unverifiedConfig = LegacyConfigConverter.insertLegacyBigQueryOptionsToConfigProto(
+        unverifiedConfig
+      );
+      if (unverifiedConfig.bigquery) {
+        checkExcessProperties(
+          (e: Error) => {
+            throw e;
+          },
+          unverifiedConfig.bigquery,
+          strictKeysOf<ILegacyTableBigqueryConfig>()([
+            "partitionBy",
+            "clusterBy",
+            "updatePartitionFilter",
+            "labels",
+            "partitionExpirationDays",
+            "requirePartitionFilter",
+            "additionalOptions"
+          ]),
+          "BigQuery table config"
+        );
+      }
+
+      // To prevent accidental data deletion, protected defaults to true if unspecified.
+      if (unverifiedConfig.protected === undefined || unverifiedConfig.protected === null) {
+        unverifiedConfig.protected = true;
       }
     }
 
@@ -496,45 +471,49 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
  * @hidden
  */
 export class IncrementalTableContext implements ITableContext {
-  constructor(private table: IncrementalTable, private isIncremental = false) {}
+  constructor(private incrementalTable: IncrementalTable, private isIncremental = false) {}
 
   public self(): string {
-    return this.resolve(this.table.proto.target);
+    return this.resolve(this.incrementalTable.proto.target);
   }
 
   public name(): string {
-    return this.table.session.finalizeName(this.table.proto.target.name);
+    return this.incrementalTable.session.finalizeName(this.incrementalTable.proto.target.name);
   }
 
   public ref(ref: Resolvable | string[], ...rest: string[]): string {
     ref = toResolvable(ref, rest);
     if (!resolvableAsTarget(ref)) {
-      this.table.session.compileError(new Error(`Action name is not specified`));
+      this.incrementalTable.session.compileError(new Error(`Action name is not specified`));
       return "";
     }
-    this.table.dependencies(ref);
+    this.incrementalTable.dependencies(ref);
     return this.resolve(ref);
   }
 
   public resolve(ref: Resolvable | string[], ...rest: string[]) {
-    return this.table.session.resolve(ref, ...rest);
+    return this.incrementalTable.session.resolve(ref, ...rest);
   }
 
   public schema(): string {
-    return this.table.session.finalizeSchema(this.table.proto.target.schema);
+    return this.incrementalTable.session.finalizeSchema(this.incrementalTable.proto.target.schema);
   }
 
   public database(): string {
-    if (!this.table.proto.target.database) {
-      this.table.session.compileError(new Error(`Warehouse does not support multiple databases`));
+    if (!this.incrementalTable.proto.target.database) {
+      this.incrementalTable.session.compileError(
+        new Error(`Warehouse does not support multiple databases`)
+      );
       return "";
     }
 
-    return this.table.session.finalizeDatabase(this.table.proto.target.database);
+    return this.incrementalTable.session.finalizeDatabase(
+      this.incrementalTable.proto.target.database
+    );
   }
 
   public where(where: Contextable<ITableContext, string>) {
-    this.table.where(where);
+    this.incrementalTable.where(where);
     return "";
   }
 
@@ -547,27 +526,27 @@ export class IncrementalTableContext implements ITableContext {
   }
 
   public preOps(statement: Contextable<ITableContext, string | string[]>) {
-    this.table.preOps(statement);
+    this.incrementalTable.preOps(statement);
     return "";
   }
 
   public postOps(statement: Contextable<ITableContext, string | string[]>) {
-    this.table.postOps(statement);
+    this.incrementalTable.postOps(statement);
     return "";
   }
 
   public disabled() {
-    this.table.disabled();
+    this.incrementalTable.disabled();
     return "";
   }
 
   public bigquery(bigquery: dataform.IBigQueryOptions) {
-    this.table.bigquery(bigquery);
+    this.incrementalTable.bigquery(bigquery);
     return "";
   }
 
   public dependencies(res: Resolvable) {
-    this.table.dependencies(res);
+    this.incrementalTable.dependencies(res);
     return "";
   }
 
@@ -580,7 +559,7 @@ export class IncrementalTableContext implements ITableContext {
   }
 
   public tags(tags: string[]) {
-    this.table.tags(tags);
+    this.incrementalTable.tags(tags);
     return "";
   }
 }

core/actions/test.ts
@@ -1,8 +1,7 @@
 import { verifyObjectMatchesProto, VerifyProtoErrorBehaviour } from "df/common/protos";
 import { StringifiedMap } from "df/common/strings/stringifier";
-import { ActionBuilder } from "df/core/actions";
+import { ActionBuilder, ITableContext } from "df/core/actions";
 import * as table from "df/core/actions/table";
-import { ITableContext } from "df/core/actions/table";
 import { View } from "df/core/actions/view";
 import { Contextable, ICommonContext, INamedConfig, Resolvable } from "df/core/common";
 import { Session } from "df/core/session";
@@ -190,7 +189,7 @@ class RefReplacingContext implements ITableContext {
     }
   }
 
-  public config(config: table.ITableConfig) {
+  public config(config: any) {
     return "";
   }
 

core/actions/view.ts
@@ -1,7 +1,6 @@
 import { verifyObjectMatchesProto, VerifyProtoErrorBehaviour } from "df/common/protos";
-import { ActionBuilder, LegacyConfigConverter } from "df/core/actions";
+import { ActionBuilder, ITableContext, LegacyConfigConverter } from "df/core/actions";
 import { Assertion } from "df/core/actions/assertion";
-import { ITableContext } from "df/core/actions/table";
 import { ColumnDescriptors } from "df/core/column_descriptors";
 import { Contextable, Resolvable } from "df/core/common";
 import * as Path from "df/core/path";
@@ -9,15 +8,27 @@ import { Session } from "df/core/session";
 import {
   actionConfigToCompiledGraphTarget,
   addDependenciesToActionDependencyTargets,
+  checkExcessProperties,
   configTargetToCompiledGraphTarget,
   nativeRequire,
   resolvableAsTarget,
   resolveActionsConfigFilename,
   setNameAndTarget,
+  strictKeysOf,
   toResolvable,
   validateQueryString
 } from "df/core/utils";
 import { dataform } from "df/protos/ts";
+
+/**
+ * @hidden
+ * This maintains backwards compatability with older versions.
+ * TODO(ekrekr): consider breaking backwards compatability of these in v4.
+ */
+export interface ILegacyViewBigqueryConfig {
+  labels: { [key: string]: string };
+  additionalOptions: { [key: string]: string };
+}
 
 /**
  * @hidden
@@ -30,10 +41,7 @@ export interface ILegacyViewConfig extends dataform.ActionConfig.ViewConfig {
   schema: string;
   fileName: string;
   type: string;
-  bigquery: {
-    labels: { [key: string]: string };
-    additionalOptions: { [key: string]: string };
-  };
+  bigquery: ILegacyViewBigqueryConfig;
   // Legacy view config's table assertions cannot directly extend the protobuf view config
   // definition because of legacy view config's flexible types.
   assertions: any;
@@ -108,9 +116,6 @@ export class View extends ActionBuilder<dataform.Table> {
     }
     if (config.disabled) {
       this.disabled();
-    }
-    if (Object.keys(config.additionalOptions).length > 0) {
-      this.bigquery({ additionalOptions: config.additionalOptions });
     }
     if (config.tags) {
       this.tags(config.tags);
@@ -418,11 +423,20 @@ export class View extends ActionBuilder<dataform.Table> {
       if (unverifiedConfig?.bigquery) {
         if (!!unverifiedConfig.bigquery.labels) {
           unverifiedConfig.labels = unverifiedConfig.bigquery.labels;
+          delete unverifiedConfig.bigquery.labels;
         }
         if (!!unverifiedConfig.bigquery.additionalOptions) {
           unverifiedConfig.additionalOptions = unverifiedConfig.bigquery.additionalOptions;
+          delete unverifiedConfig.bigquery.additionalOptions;
         }
-        delete unverifiedConfig.bigquery;
+        checkExcessProperties(
+          (e: Error) => {
+            throw e;
+          },
+          unverifiedConfig.bigquery,
+          strictKeysOf<ILegacyViewBigqueryConfig>()(["labels", "additionalOptions"]),
+          "BigQuery view config"
+        );
       }
     }
 
@@ -438,45 +452,45 @@ export class View extends ActionBuilder<dataform.Table> {
  * @hidden
  */
 export class ViewContext implements ITableContext {
-  constructor(private table: View, private isIncremental = false) {}
+  constructor(private view: View, private isIncremental = false) {}
 
   public self(): string {
-    return this.resolve(this.table.proto.target);
+    return this.resolve(this.view.proto.target);
   }
 
   public name(): string {
-    return this.table.session.finalizeName(this.table.proto.target.name);
+    return this.view.session.finalizeName(this.view.proto.target.name);
   }
 
   public ref(ref: Resolvable | string[], ...rest: string[]): string {
     ref = toResolvable(ref, rest);
     if (!resolvableAsTarget(ref)) {
-      this.table.session.compileError(new Error(`Action name is not specified`));
+      this.view.session.compileError(new Error(`Action name is not specified`));
       return "";
     }
-    this.table.dependencies(ref);
+    this.view.dependencies(ref);
     return this.resolve(ref);
   }
 
   public resolve(ref: Resolvable | string[], ...rest: string[]) {
-    return this.table.session.resolve(ref, ...rest);
+    return this.view.session.resolve(ref, ...rest);
   }
 
   public schema(): string {
-    return this.table.session.finalizeSchema(this.table.proto.target.schema);
+    return this.view.session.finalizeSchema(this.view.proto.target.schema);
   }
 
   public database(): string {
-    if (!this.table.proto.target.database) {
-      this.table.session.compileError(new Error(`Warehouse does not support multiple databases`));
+    if (!this.view.proto.target.database) {
+      this.view.session.compileError(new Error(`Warehouse does not support multiple databases`));
       return "";
     }
 
-    return this.table.session.finalizeDatabase(this.table.proto.target.database);
+    return this.view.session.finalizeDatabase(this.view.proto.target.database);
   }
 
   public where(where: Contextable<ITableContext, string>) {
-    this.table.where(where);
+    this.view.where(where);
     return "";
   }
 
@@ -489,27 +503,27 @@ export class ViewContext implements ITableContext {
   }
 
   public preOps(statement: Contextable<ITableContext, string | string[]>) {
-    this.table.preOps(statement);
+    this.view.preOps(statement);
     return "";
   }
 
   public postOps(statement: Contextable<ITableContext, string | string[]>) {
-    this.table.postOps(statement);
+    this.view.postOps(statement);
     return "";
   }
 
   public disabled() {
-    this.table.disabled();
+    this.view.disabled();
     return "";
   }
 
   public bigquery(bigquery: dataform.IBigQueryOptions) {
-    this.table.bigquery(bigquery);
+    this.view.bigquery(bigquery);
     return "";
   }
 
   public dependencies(res: Resolvable) {
-    this.table.dependencies(res);
+    this.view.dependencies(res);
     return "";
   }
 
@@ -522,7 +536,7 @@ export class ViewContext implements ITableContext {
   }
 
   public tags(tags: string[]) {
-    this.table.tags(tags);
+    this.view.tags(tags);
     return "";
   }
 }

core/common.ts
@@ -1,5 +1,7 @@
 import { strictKeysOf } from "df/core/utils";
 
+// TODO(ekrekr): remove deprecated items from this file.
+
 /**
  * Context methods are available when evaluating contextable SQL code, such as
  * within SQLX files, or when using a [Contextable](#Contextable) argument with the JS API.

commit 939f2b8fcd81a253923e611baa188e46b02da1c1

Fix LegacyConfigConverter mutating caller-shared configs
Shallow-clone .bigquery and .assertions before in-place coercions so
`publish("t1", shared); publish("t2", shared);` no longer leaves t2/t3
with an empty bigquery block. Regression from #1780 (3.0.10).

core/actions/index.ts

core/actions/index.ts
@@ -419,6 +419,8 @@ export class LegacyConfigConverter {
     // Type `any` is used here to facilitate the type hacking for legacy compatibility.
     const legacyConfig: any = unverifiedConfig;
     if (legacyConfig?.assertions) {
+      // Shallow-clone before in-place coercions so we don't mutate a caller-shared object.
+      legacyConfig.assertions = { ...legacyConfig.assertions };
       if (typeof legacyConfig.assertions?.uniqueKey === "string") {
         legacyConfig.assertions.uniqueKey = [legacyConfig.assertions.uniqueKey];
       }
@@ -444,6 +446,8 @@ export class LegacyConfigConverter {
     if (!legacyConfig?.bigquery) {
       return legacyConfig;
     }
+    // Shallow-clone before the delete-loop below so we don't mutate a caller-shared object.
+    legacyConfig.bigquery = { ...legacyConfig.bigquery };
     if (!!legacyConfig.bigquery.partitionBy) {
       legacyConfig.partitionBy = legacyConfig.bigquery.partitionBy;
       delete legacyConfig.bigquery.partitionBy;