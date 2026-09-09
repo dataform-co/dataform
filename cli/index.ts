7f5efc36cc7610dbf9ff86440be6bf10b5205a0e
Marcin Biernacik <marcin.biernacik@gmail.com>
2026-09-09 14:50:01 +0000

    refactor(cli): clean up help command handling and encapsulate yargs configuration (#2290)

--- cli/index.ts
+++ cli/index.ts
@@ -1,9 +1,7 @@
-import yargs from "yargs";
-
 import {
   compileCommand,
+  createHelpCommand,
   formatCommand,
-  helpCommand,
   initCommand,
   initCredsCommand,
   installCommand,
@@ -11,45 +9,24 @@ import {
   testCommand
 } from "df/cli/commands";
 import { printError } from "df/cli/console";
-import { createYargsCli } from "df/cli/yargswrapper";
+import { createYargsCli, ICommand } from "df/cli/yargswrapper";
 
 process.on("unhandledRejection", async (reason: any) => {
   printError(`Unhandled promise rejection: ${reason?.stack || reason}`);
 });
 
-// TODO: Since yargs launched an actually well typed API in version 12, let's use it as this file is currently not type checked.
 export function runCli() {
-  const builtYargs = createYargsCli({
-    commands: [
-      helpCommand,
-      initCommand,
-      installCommand,
-      initCredsCommand,
-      compileCommand,
-      testCommand,
-      runCommand,
-      formatCommand
-    ]
-  })
-    .scriptName("dataform")
-    .strict()
-    .wrap(null)
-    .recommendCommands()
-    .fail(async (msg: string, err: any) => {
-      if (!!err && err.name === "VMError" && err.message.includes("Cannot find module")) {
-        printError("Could not find NPM dependencies. Have you run 'dataform install'?");
-      } else {
-        const message = err?.message ? err.message.split("\n")[0] : msg;
-        printError(`Dataform encountered an error: ${message}`);
-        if (err?.stack) {
-          printError(err.stack);
-        }
-      }
-      process.exit(1);
-    }).argv;
+  const commands: ICommand[] = [
+    initCommand,
+    installCommand,
+    initCredsCommand,
+    compileCommand,
+    testCommand,
+    runCommand,
+    formatCommand
+  ];
 
-  // If no command is specified, show top-level help string.
-  if (!builtYargs._[0]) {
-    yargs.showHelp();
-  }
+  createYargsCli({
+    commands: [createHelpCommand(commands), ...commands]
+  });
 }
