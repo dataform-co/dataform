94463510fc14d1e419499992f4f59c423ba423e3
Mohamed M. Saad <33448385+mohamed-saad@users.noreply.github.com>
2026-09-17 20:41:20 +0200

    Preserve Governance Controls in Incremental Tables. (#2314)
    * Preserve Governance Controls in Incremental Tables.
    
    Adding flags to enable preserving governance controls such as Data
    Policies, and Data Governance Tags during the incremental tables full
    refresh operation.
    
    * Fix failing test

--- core/actions/incremental_table.ts
+++ core/actions/incremental_table.ts
@@ -203,6 +203,7 @@ export class IncrementalTable extends ActionBuilder<dataform.Table> {
       requirePartitionFilter: config.requirePartitionFilter,
       additionalOptions: config.additionalOptions,
       incrementalPredicates: config.incrementalPredicates,
+      preserveGovernanceControls: config.preserveGovernanceControls ?? session.projectConfig.preserveGovernanceControls ?? false,
       ...(config.iceberg ? {
         connection: getConnectionForIcebergTable(
           config.iceberg.connection,
--- core/main_extensions_test.ts
+++ core/main_extensions_test.ts
@@ -194,6 +194,25 @@ select 1 as a`
     expect(result.compile.compiledGraph.tables[0].bigquery.preserveGovernanceControls).equals(true);
   });
 
+
+  test("preserveGovernanceControls in Incremental Tables propagates to compiled graph", () => {
+    const projectDir = tmpDirFixture.createNewTmpDir();
+    writeWorkflowSettingsFile(projectDir, VALID_WORKFLOW_SETTINGS_YAML);
+    writeDefinitionFile(
+      projectDir,
+      "file.sqlx",
+      `
+config {
+  type: "incremental",
+  preserveGovernanceControls: true
+}
+select 1 as a`
+    );
+    const result = runMainInVm(coreExecutionRequestFromPath(projectDir));
+    expect(result.compile.compiledGraph.graphErrors.compilationErrors).deep.equals([]);
+    expect(result.compile.compiledGraph.tables[0].bigquery.preserveGovernanceControls).equals(true);
+  });
+
   test("preserveGovernanceControls in workflow_settings.yaml propagates to compiled graph", () => {
     const projectDir = tmpDirFixture.createNewTmpDir();
     writeWorkflowSettingsFile(
